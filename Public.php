60b5316df2b50e40b454723fed96a64bce3bda41
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-02-08 10:40:06 +0100

    Adds check function
    Indicates that the plugin is running

--- Public.php
+++ Public.php
@@ -1,3 +1,10 @@
 <?php
 
 // Public functions
+
+if(!function_exists('component_library')) {
+  function component_library() {
+    return true; 
+    //Tells other plugins that this is activated. 
+  }
+}
