ef7563acd3698aa5b17b2a9724e47d2648a0ae1d
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-02-23 13:51:23 +0100

    Fixed so signature handles dates better

--- source/php/Component/Signature/signature.blade.php
+++ source/php/Component/Signature/signature.blade.php
@@ -1,49 +1,57 @@
 <{{$componentElement}} id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
     
-    @avatar([
-        'image' => ($avatar) ? $avatar : false,
-        'name' => $author,
-        'size' => $avatar_size
-    ])
-    @endavatar
+    @if($author)
+        @avatar([
+            'image' => ($avatar) ? $avatar : false,
+            'name' => $author,
+            'size' => $avatar_size,
+            "classList" => [
+                $baseClass.'__avatar'
+            ]
+        ])
+        @endavatar
 
-    <div class="{{$baseClass}}__content">
+        <div class="{{$baseClass}}__author-box">
+            <div>
+                @typography([
+                    "element" => "span",
+                    "variant" => "subtitle",
+                    "classList" => [
+                        $baseClass.'__author'
+                    ]
+                ])
+                    {{$author}}
+                @endtypography
+
+                @if($authorRole ) 
+                    @typography([
+                        "element" => "span",
+                        "variant" => "byline",
+                        "classList" => [
+                            $baseClass.'__title'
+                        ]
+                    ])
+                        {{$authorRole}}
+                    @endtypography
+                @endif
+            </div>
+        </div>
+    @endif
+
+    
+    @if($published)
+    <div class="{{$baseClass}}__dates {{$author ? $baseClass.'__dates--aligned' : ''}}">
+
+        @typography([
+            "element" => "span",
+            "variant" => "byline",
+            "classList" => [
+                $baseClass.'__published'
+            ]
+        ])
+            {{$label->publish}}: @date(['action' => 'formatDate','timestamp' => $published])@enddate
+        @endtypography
         
-        @if($author)
-            @typography([
-                "element" => "span",
-                "variant" => "subtitle",
-                "classList" => [
-                    $baseClass.'__author'
-                ]
-            ])
-                {{$author}}
-            @endtypography
-        @endif
-
-        @if($authorRole && $author) 
-            @typography([
-                "element" => "span",
-                "variant" => "byline",
-                "classList" => [
-                    $baseClass.'__title'
-                ]
-            ])
-                {{$authorRole}}
-            @endtypography
-        @endif
-
-        @if ($published)
-            @typography([
-                "element" => "span",
-                "variant" => "byline",
-                "classList" => [
-                    $baseClass.'__published'
-                ]
-            ])
-                {{$label->publish}}: @date(['action' => 'formatDate','timestamp' => $published])@enddate
-            @endtypography
-        @endif
 
         @if ($updated)
             @typography([
@@ -57,4 +65,6 @@
             @endtypography
         @endif
     </div>
+    @endif
+
 </{{$componentElement}}>
