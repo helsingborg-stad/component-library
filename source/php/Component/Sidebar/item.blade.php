3c6c4a6a580aa9e1937bfa4a9eb94bd2611538d0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-04-12 14:10:17 +0200

    Check if items array exists

--- source/php/Component/Sidebar/item.blade.php
+++ source/php/Component/Sidebar/item.blade.php
@@ -5,45 +5,47 @@
         js-toggle-item="{{$appendID}}"
         js-toggle-class="{{$baseClass}}__item--is-expanded">
 @endif
+    
+    @if(is_array($items) && !empty($items))
+        @foreach ($items as $item)
 
-    @foreach ($items as $item)
+            <div class="{{$baseClass}}__item">
+                <a  class="{{$baseClass}}__link" 
+                    href="{{$item['href']}}" 
+                    aria-label="{{$item['label']}}" 
+                    item-ancestor="{{(int) $item['ancestor']}}" 
+                    item-active="{{(int) $item['active']}}"
+                    item-has-children="{{(int) $item['children']}}"
+                    item-children-async="{{ (int) is_bool($item['children'])}}"
+                >
+                    {{$item['label']}}
+                </a>
 
-        <div class="{{$baseClass}}__item">
-            <a  class="{{$baseClass}}__link" 
-                href="{{$item['href']}}" 
-                aria-label="{{$item['label']}}" 
-                item-ancestor="{{(int) $item['ancestor']}}" 
-                item-active="{{(int) $item['active']}}"
-                item-has-children="{{(int) $item['children']}}"
-                item-children-async="{{ (int) is_bool($item['children'])}}"
-            >
-                {{$item['label']}}
-            </a>
+                @if ($item['children'])
 
-            @if ($item['children'])
+                    @php $rndId = uniqid(); @endphp
 
-                @php $rndId = uniqid(); @endphp
+                    <div class="{{$baseClass}}__toggle"
+                        js-toggle-trigger="{{$rndId}}"
+                        aria-label="{{$item['id']}}"
+                        aria-pressed="{{ ( $item['active'] || $item['ancestor'] ) ? 'true' : 'false' }}">
 
-                <div class="{{$baseClass}}__toggle"
-                    js-toggle-trigger="{{$rndId}}"
-                    aria-label="{{$item['id']}}"
-                    aria-pressed="{{ ( $item['active'] || $item['ancestor'] ) ? 'true' : 'false' }}">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </div>
 
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                </div>
-
-                @include (
-                    'Sidebar.item', 
-                    array(
-                        'items' => $item['children'], 
-                        'appendID' => $rndId, 
-                        'top_level' => false,
-                        'isExpanded' => ( boolval($item['active']) || boolval($item['ancestor']) ) ? true : false 
+                    @include (
+                        'Sidebar.item', 
+                        array(
+                            'items' => $item['children'], 
+                            'appendID' => $rndId, 
+                            'top_level' => false,
+                            'isExpanded' => ( boolval($item['active']) || boolval($item['ancestor']) ) ? true : false 
+                        )
                     )
-                )
-                
-            @endif
-        </div>
-    @endforeach
+                    
+                @endif
+            </div>
+        @endforeach
+    @endif
 </div>
