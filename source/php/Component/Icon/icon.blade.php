72ae51f105136051e09dc5b56c5e440a4e8f0f1a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-06-16 12:23:49 +0200

    hotfix: do not translate icon strings in automatic translation tools.

--- source/php/Component/Icon/icon.blade.php
+++ source/php/Component/Icon/icon.blade.php
@@ -1,6 +1,6 @@
 <!-- icon.blade.php -->
 @if($icon)
-    <{{$componentElement}} id="{{ $id }}" class="{{ $class }} material-icons" {!! $attribute !!}>
+    <{{$componentElement}} id="{{ $id }}" class="{{ $class }} material-icons" {!! $attribute !!} translate="no">
         @if($icon)
             {{$icon}}
         @else 
