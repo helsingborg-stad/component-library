ae38088f93516765cd5768d661487806860cc2e8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-06-30 10:05:18 +0200

    Update structure & add image classlist

--- source/php/Component/Segment/segment.blade.php
+++ source/php/Component/Segment/segment.blade.php
@@ -2,35 +2,48 @@
 <section id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
 
     @if ($image)
-    <img class="{{$baseClass}}__image" src="{{$image}}"/>
+        <img class="{{$baseClass}}__image {{ $imageClass }}" src="{{$image}}" alt=""/>
     @endif
 
     <div class="{{$baseClass}}__content o-container o-container--content o-container--keep-spacing">
-        @if($title)
-        @typography([
-            "element" => "h2",
-            "variant" => "h1",
-            "classList" => [$baseClass . '__title'],
-        ])
-            {{ $title }}
-        @endtypography
-        @endif
+    
+        @if($title||$content)
+            <div class="{{$baseClass}}__padder">
+                
+                @if($title)
+                    @typography([
+                        "element" => "h2",
+                        "variant" => "h1",
+                        "classList" => [$baseClass . '__title'],
+                    ])
+                        {{ $title }}
+                    @endtypography
+                @endif
 
-        @if($content)
-        @typography([
-            "variant" => "p",
-            "classList" => [$baseClass . '__text'],
-        ])
-            {{ $content }}
-        @endtypography
+                @if($content)
+                    @typography([
+                        "variant" => "p",
+                        "classList" => [$baseClass . '__text'],
+                    ])
+                        {{ $content }}
+                    @endtypography
+                @endif
+
+                @if ($slotHasData && $layout == 'split')
+                    <div class="{{$baseClass}}__slot">
+                        {{ $slot }}
+                    </div>
+                @endif
+
+            </div>
         @endif
 
     </div>
     
-    @if ($slotHasData)
-    <div class="{{$baseClass}}__slot o-container o-container--wide">
-        {{ $slot }}
-    </div>
+    @if ($slotHasData && $layout == 'full-width')
+        <div class="{{$baseClass}}__slot o-container">
+            {{ $slot }}
+        </div>
     @endif
     
 
