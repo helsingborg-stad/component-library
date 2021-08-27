2b7a2f22bfe06e6b6f40053f38f2fa08473065f5
Alexander Boman Skoug <39676080+alexanderbomanskoug2@users.noreply.github.com>
2021-08-27 14:42:53 +0200

    - add new options for background color and text color (#30)

--- source/php/Component/Header/Header.php
+++ source/php/Component/Header/Header.php
@@ -8,5 +8,13 @@ class Header extends \ComponentLibrary\Component\BaseController
         //Extract array for eazy access (fetch only)
         extract($this->data);
 
+        if ($backgroundColor) {
+            $this->data['classList'][] = "{$this->getBaseClass()}--{$backgroundColor}";
+        }
+
+        if ($textColor) {
+            $this->data['classList'][] = "{$this->getBaseClass()}--text-{$textColor}";
+        }
+
     }
 }
