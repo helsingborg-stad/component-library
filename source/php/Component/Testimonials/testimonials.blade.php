895865f1b63d843018f3d285b6a989337cb1a621
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-09-09 10:15:38 +0200

    - add attributes for carousel functionality

--- source/php/Component/Testimonials/testimonials.blade.php
+++ source/php/Component/Testimonials/testimonials.blade.php
@@ -1,6 +1,6 @@
 <!-- testimonials.blade.php -->
 @if($testimonials)
-<div class="{{$baseClass .'s__wrapper'}} {{$wrapperClassList}}">
+<div class="{{$baseClass .'s__wrapper'}} {{$wrapperClassList}}"  {!! $wrapperAttributeList !!} style="transform: translateX(0%);">
     @foreach($testimonials as $testimonial)
         <{{ $componentElement }} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
             @include('Testimonials.partials.item')    
@@ -15,6 +15,9 @@
         'size' => 'lg',
         'classList' => [
             $baseClass . 's__button'
+        ],
+        'attributeList' => [
+            'js-testimonials__back' => ''
         ]
     ])
     @endbutton
@@ -24,6 +27,9 @@
         'size' => 'lg',
         'classList' => [
             $baseClass . 's__button',
+        ],
+        'attributeList' => [
+            'js-testimonials__forward' => ''
         ]
     ])
     @endbutton
