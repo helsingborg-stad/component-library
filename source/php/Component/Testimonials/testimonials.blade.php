b8e6e53f5bfc022fc1cf705dd5b757a3cb8c4c92
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-09-10 13:36:12 +0200

    - add modifications to styling and script to support browser

--- source/php/Component/Testimonials/testimonials.blade.php
+++ source/php/Component/Testimonials/testimonials.blade.php
@@ -1,37 +1,39 @@
 <!-- testimonials.blade.php -->
 @if($testimonials)
-<div class="{{$baseClass .'s__wrapper'}} {{$wrapperClassList}}"  {!! $wrapperAttributeList !!} style="transform: translateX(0%);">
-    @foreach($testimonials as $testimonial)
-        <{{ $componentElement }} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
-            @include('Testimonials.partials.item')    
-        </{{ $componentElement }}>
-    @endforeach
-</div>
+<div class="{{$baseClass .'s'}}">
+    <div class="{{$baseClass .'s__wrapper'}} {{$wrapperClassList}}"  {!! $wrapperAttributeList !!} style="transform: translateX(0%);">
+        @foreach($testimonials as $testimonial)
+            <{{ $componentElement }} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
+                @include('Testimonials.partials.item')    
+            </{{ $componentElement }}>
+        @endforeach
+    </div>
 
-@if (count($testimonials) > 1 && $isCarousel)
-    @button([
-        'style' => 'filled',
-        'icon' => 'arrow_back',
-        'size' => 'lg',
-        'classList' => [
-            $baseClass . 's__button'
-        ],
-        'attributeList' => [
-            'js-testimonials__back' => ''
-        ]
-    ])
-    @endbutton
-    @button([
-        'style' => 'filled',
-        'icon' => 'arrow_forward',
-        'size' => 'lg',
-        'classList' => [
-            $baseClass . 's__button',
-        ],
-        'attributeList' => [
-            'js-testimonials__forward' => ''
-        ]
-    ])
-    @endbutton
-@endif
+    @if (count($testimonials) > 1 && $isCarousel)
+        @button([
+            'style' => 'filled',
+            'icon' => 'arrow_back',
+            'size' => 'lg',
+            'classList' => [
+                $baseClass . 's__button'
+            ],
+            'attributeList' => [
+                'js-testimonials__back' => ''
+            ]
+        ])
+        @endbutton
+        @button([
+            'style' => 'filled',
+            'icon' => 'arrow_forward',
+            'size' => 'lg',
+            'classList' => [
+                $baseClass . 's__button',
+            ],
+            'attributeList' => [
+                'js-testimonials__forward' => ''
+            ]
+        ])
+        @endbutton
+    @endif
+</div>
 @endif
