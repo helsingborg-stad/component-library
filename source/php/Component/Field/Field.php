6bfd2dbaa73bb99d665cdc4df079100b19e6545a
nRamstedt <nikolas.ramstedt@gmail.com>
2021-12-03 09:33:00 +0100

    fix: formatting

--- source/php/Component/Field/Field.php
+++ source/php/Component/Field/Field.php
@@ -9,7 +9,7 @@ class Field extends \ComponentLibrary\Component\Form\Form
         //Extract array for eazy access (fetch only)
         extract($this->data);
 
-        if(!$id) {
+        if (!$id) {
             $this->data['id'] = uniqid();
         }
 
@@ -23,30 +23,30 @@ class Field extends \ComponentLibrary\Component\Form\Form
 
         $this->data['showLabel'] = !$hideLabel && !empty($label);
 
-        if($this->data['showLabel']) {
+        if ($this->data['showLabel']) {
             $this->data['attributeList']['aria-labelledby'] = 'label_' . $this->data['id'];
         } else {
             $this->data['attributeList']['aria-label'] = $label;
         }
 
         //Set type
-        $this->data['classList'][] = $this->getBaseClass() . "__" . $type; 
+        $this->data['classList'][] = $this->getBaseClass() . "__" . $type;
 
         //Handle icon
-        $this->data['icon'] = $this->getIcon($icon, $size); 
-        if($this->data['icon']) {
-            $this->data['classList'][] = $this->getBaseClass() . "--icon"; 
+        $this->data['icon'] = $this->getIcon($icon, $size);
+        if ($this->data['icon']) {
+            $this->data['classList'][] = $this->getBaseClass() . "--icon";
         }
 
         //Handle size
-        if(!in_array($size, ['sm', 'md', 'lg'])) {
-            $size = "md"; 
+        if (!in_array($size, ['sm', 'md', 'lg'])) {
+            $size = "md";
         }
-        $this->data['classList'][] = $this->getBaseClass() . "--" . $size; 
-        
+        $this->data['classList'][] = $this->getBaseClass() . "--" . $size;
+
         //Handle radius
-        if($this->data['radius']) {
-            $this->data['classList'][] = $this->getBaseClass() . "--radius-" . $this->data['radius']; 
+        if ($this->data['radius']) {
+            $this->data['classList'][] = $this->getBaseClass() . "--radius-" . $this->data['radius'];
         }
 
         //Set data
@@ -54,14 +54,13 @@ class Field extends \ComponentLibrary\Component\Form\Form
 
         //Hsandle datepicker exceptions
         if ($type == 'datepicker') {
-
             $this->compParams = [
                 'type' => 'text'
             ];
             $this->data['type'] = 'text';
 
             $this->data['attributeList']['js-datepicker'] = true;
-    
+
             $this->setMinAndMaxDate(isset($datepicker['minDate']) ? $datepicker['minDate'] : '', isset($datepicker['maxDate']) ? $datepicker['maxDate'] : '');
 
             $this->setOptionals(isset($datepicker['title']) ? $datepicker['title'] : 'Select a date');
@@ -74,25 +73,24 @@ class Field extends \ComponentLibrary\Component\Form\Form
                 'hideOnSelect' => isset($datepicker['hideOnSelect']) ? $datepicker['hideOnSelect'] : true,
             ]);
         }
-        
     }
 
     /**
-     * Get a icon, set default vars. 
+     * Get a icon, set default vars.
      *
      * @param array|bool $icon
      * @return array|bool
      */
-    public function getIcon($icon, $size) {
-        
-        if(is_array($icon) && !empty($icon)) {
+    public function getIcon($icon, $size)
+    {
+        if (is_array($icon) && !empty($icon)) {
             return array_merge([
                 'size' => $this->getIconSize($size),
                 'icon' => 'search'
-            ], $icon); 
+            ], $icon);
         }
 
-        return false; 
+        return false;
     }
 
     /**
@@ -101,18 +99,20 @@ class Field extends \ComponentLibrary\Component\Form\Form
      * @param string $fieldSize
      * @return string
      */
-    public function getIconSize($fieldSize) {
+    public function getIconSize($fieldSize)
+    {
         switch ($fieldSize) {
             case 'sm':
-                return 'sm'; 
+                return 'sm';
                 break;
             default:
                 return 'md';
-            break;  
+            break;
         }
     }
 
-    public function setData(){
+    public function setData()
+    {
         $this->data['label'] = $this->compParams['label'];
         $this->data['type'] = $this->compParams['type'];
         $this->data['required'] = $this->compParams['required'];
@@ -120,7 +120,8 @@ class Field extends \ComponentLibrary\Component\Form\Form
         $this->data['value'] = $this->compParams['value'];
     }
 
-    public function setMinAndMaxDate($minDate, $maxDate) {
+    public function setMinAndMaxDate($minDate, $maxDate)
+    {
         $minDate ?
             $this->data['attributeList']['c-datepicker-min'] = date("n/j/Y", strtotime($minDate))
             : '';
@@ -129,20 +130,22 @@ class Field extends \ComponentLibrary\Component\Form\Form
             : '';
     }
 
-    public function setOptionals($title) {
+    public function setOptionals($title)
+    {
         $this->data['attributeList']['c-datepicker-title'] = $title;
     }
 
-    public function setRequired($required) {
+    public function setRequired($required)
+    {
         $this->data['attributeList']['c-datepicker-required'] = $required;
     }
 
-    public function buildUI($UI) {
+    public function buildUI($UI)
+    {
         $this->data['attributeList']['c-datepicker-showResetButton'] = $UI['showResetButton'];
         $this->data['attributeList']['c-datepicker-showDaysOutOfMonth'] = $UI['showDaysOutOfMonth'];
         $this->data['attributeList']['c-datepicker-showClearButton'] = $UI['showClearButton'];
         $this->data['attributeList']['c-datepicker-hideOnBlur'] = $UI['hideOnBlur'];
         $this->data['attributeList']['c-datepicker-hideOnSelect'] = $UI['hideOnSelect'];
     }
-
 }
