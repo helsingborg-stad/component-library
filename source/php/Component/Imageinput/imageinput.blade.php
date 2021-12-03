f0b829917d7be489c745543043d8e2e9f3f6332e
Freya <752642+faejr@users.noreply.github.com>
2021-12-03 16:59:51 +0100

    WIP: Create image input field

--- source/php/Component/Imageinput/imageinput.blade.php
+++ source/php/Component/Imageinput/imageinput.blade.php
@@ -1,13 +1,21 @@
-@fileinput([
-'id' => $id . '-input',
-'name' => $name,
-'display' => 'area',
-'multiple' => $multiple,
-'label' => __('Upload an image', 'event-integration'),
-'accept' => "image/gif, image/jpeg, image/png",
-'attributeList' => [
-'required' => 'required'
-]
-])
-test
-@endfileinput
+<div class="{{$baseClass}}">
+    <div id="{{ $id }}" class="{{$baseClass}}__preview">
+        <div class="{{$baseClass}}__image material-icons">
+            <img src="" style="display:none" />
+        </div>
+        <span>Bildnamn.jpg, 1200*700px, 234kb</span>
+    </div>
+    @fileinput([
+        'id' => $id . '-input',
+        'name' => $name,
+        'display' => 'area',
+        'multiple' => $multiple,
+        'label' => __('Upload an image', 'event-integration'),
+        'accept' => "image/gif, image/jpeg, image/png",
+        'attributeList' => [
+            'required' => 'required',
+            'data-image-preview' => $id
+        ]
+    ])
+    @endfileinput
+</div>
