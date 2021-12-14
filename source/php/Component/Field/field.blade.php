965d1c6877e8a13fd4031352fab5194bde9ff299
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-12-14 13:24:57 +0100

    fix: field markup with helper text

--- source/php/Component/Field/field.blade.php
+++ source/php/Component/Field/field.blade.php
@@ -1,35 +1,29 @@
 <!-- field.blade.php -->
-@if($showLabel)
-<label id="label_{{ $id }}" class="c-field__text--label" for="input_{{ $id }}">{{$label}}</label>
-@endif
-
-<div id="{{ $id }}" class="{{$class}}">
+<div class="{{$class}}" id="{{ $id }}">
 
     @if($icon)
         @icon($icon)
         @endicon
     @endif
 
-    <input id="input_{{ $id }}"
-           value="{{$value}}"
-           {!! $attribute !!}
-           @if($required)
-               required
-               data-required="1"
-               aria-required="true"
-           @endif
-           placeholder="{{$placeholder}}"
-    />
-    <label class="c-field__text--placeholder">
-        {{$placeholder}}
-    </label>
 
-    <div id="error_input_{{ $id }}_message" class="c-field__input-invalid-message">
-        @icon([
-            'icon' => 'error',
-            'size' => 'sm'
-        ])
-        @endicon
-        <span class="errorText"></span>
-    </div>
+    @if(!empty($label))
+        <label class="{{$baseClass}}__label" for="input_{{ $id }}" id="label_{{ $id }}">{{$label}}</label>
+    @endif
+
+
+
+        <input id="input_{{ $id }}"
+            value="{{$value}}"
+            {!! $attribute !!}
+            @if($required)
+                required
+                data-required="1"
+                aria-required="true"
+            @endif
+            placeholder="{{$placeholder}}"
+        />
+
+
+    <small class="{{$baseClass}}__helper">{{$helperText}}</small>
 </div>
