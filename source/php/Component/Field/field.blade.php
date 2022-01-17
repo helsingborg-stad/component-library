045110478306cd70eef8f082f8d19d13c8a848ae
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-01-17 17:24:50 +0100

    fix: add inner wrapper & move icon within wrapper

--- source/php/Component/Field/field.blade.php
+++ source/php/Component/Field/field.blade.php
@@ -1,38 +1,38 @@
 <!-- field.blade.php -->
 <div class="{{$class}}" id="{{ $id }}">
-
-    @if(!empty($icon))
-        @icon(array_merge(['classList' => [$baseClass . '__icon']], $icon))
-        @endicon
-    @endif
-
     @if(!empty($label) && !$hideLabel)
         <label class="{{$baseClass}}__label" for="input_{{ $id }}" id="label_{{ $id }}">{{$label}}</label>
     @endif
+    
+    <div class="{{$baseClass}}__inner">
+        @if(!empty($icon))
+            @icon(array_merge(['classList' => [$baseClass . '__icon']], $icon))
+            @endicon
+        @endif
 
-    @if($multiline)
-        <textarea id="input_{{ $id }}"
-            {!! $attribute !!}
-            @if($required)
-                required
-                data-required="1"
-                aria-required="true"
-            @endif
-            placeholder="{{$placeholder}}"
-        >{{$value ?? null}}</textarea>
-    @else
-        <input id="input_{{ $id }}"
-            value="{{$value}}"
-            {!! $attribute !!}
-            @if($required)
-                required
-                data-required="1"
-                aria-required="true"
-            @endif
-            placeholder="{{$placeholder}}"
-        />
-    @endif
-
+        @if($multiline)
+            <textarea id="input_{{ $id }}"
+                {!! $attribute !!}
+                @if($required)
+                    required
+                    data-required="1"
+                    aria-required="true"
+                @endif
+                placeholder="{{$placeholder}}"
+            >{{$value ?? null}}</textarea>
+        @else
+            <input id="input_{{ $id }}"
+                value="{{$value}}"
+                {!! $attribute !!}
+                @if($required)
+                    required
+                    data-required="1"
+                    aria-required="true"
+                @endif
+                placeholder="{{$placeholder}}"
+            />
+        @endif
+    </div>
     @if ($helperText)
         <small class="{{$baseClass}}__helper">{{$helperText}}</small>
     @endif
