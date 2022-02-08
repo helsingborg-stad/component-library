2aa5d0dbd8e1703ee985eead1a30a8dce3b5914d
Erik Granström <erik.granstrom@helsingborg.se>
2022-02-08 14:31:59 +0100

    add support for field label

--- source/php/Component/Select/select.blade.php
+++ source/php/Component/Select/select.blade.php
@@ -1,20 +1,30 @@
 <div class="c-select__container c-field">
     @if($label)
-    <label for="{{ $id }}" class="u-sr__only c-field__text--label">{{$label}}</label>
+        <label for="{{ $id }}" class="c-field__label">{{$label}}</label>
     @endif
+    
+    <div class="u-position--relative">
+        <select id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
+            @if($label)
+            <option class="c-select__option" {{ $preselected === '' ? 'selected' : ''}} value="">{!!$label!!}</option>
+            @endif
 
-    <select id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
-        @if($label)
-        <option class="c-select__option" {{ $preselected === '' ? 'selected' : ''}} value="">{!!$label!!}</option>
-        @endif
 
+            @foreach ($options as $value => $name)
+                <option class="c-select__option" value="{!!$value!!}" {{ $preselected === $value ? 'selected' : ''}}>{!!$name!!}</option>
+            @endforeach
 
-        @foreach ($options as $value => $name)
-            <option class="c-select__option" value="{!!$value!!}" {{ $preselected === $value ? 'selected' : ''}}>{!!$name!!}</option>
-        @endforeach
-
-        {!! $slot !!}
-    </select>
+            {!! $slot !!}
+        </select>
+        @icon([
+            'classList' => [
+                'c-select__icon'
+            ],
+            'icon' => 'expand_more',
+            'size' => 'md',
+        ])
+        @endicon
+    </div>
 
     <div id="error_input_{{ $id }}_message" class="c-field__input-invalid-message">
         @icon([
@@ -24,14 +34,7 @@
         @endicon
         <span class="errorText"></span>
     </div>
-
-    @icon([
-        'classList' => [
-            'c-select__icon'
-        ],
-        'icon' => 'expand_more',
-        'size' => 'md',
-    ])
-    @endicon
-
+    @if ($helperText)
+        <small class="c-field__helper">{{$helperText}}</small>
+    @endif
 </div>
