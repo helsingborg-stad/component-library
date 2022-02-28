8256d941ce259750244f128c08715cece9d527e9
Freya <752642+faejr@users.noreply.github.com>
2022-02-28 12:48:47 +0100

    fix: always display background

--- source/php/Component/Block/views/base.blade.php
+++ source/php/Component/Block/views/base.blade.php
@@ -1,3 +1,3 @@
-@includeWhen($image && $image['src'], 'Block.components.image')
+@includeWhen($image, 'Block.components.image')
 @includeWhen($heading || $subHeading || $content || $collapsible, 'Block.partials.body')
 @includeWhen($tags || $buttons, 'Block.partials.footer')
