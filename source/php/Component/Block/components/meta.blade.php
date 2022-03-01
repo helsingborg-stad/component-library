e09bb32d9f9f34c24f3e63edec6b2a022494ad60
Freya <752642+faejr@users.noreply.github.com>
2022-03-01 14:20:55 +0100

    fix: meta can display links

--- source/php/Component/Block/components/meta.blade.php
+++ source/php/Component/Block/components/meta.blade.php
@@ -1,3 +1,19 @@
 @typography(['variant' => 'meta', 'element' => 'span'])
-    {!! $meta !!}
+    @if(is_array($meta))
+        @if(array_filter($meta, 'is_string') !== [])
+            {{ implode(', ', $meta) }}
+        @else
+            @foreach($meta as $value)
+                @if(isset($value['href']) && !empty($value['href']))
+                    @link(['href' => $value['href']])
+                        {!! $value['label'] !!}
+                    @endlink
+                @else
+                    {!! $value['label'] !!}
+                @endif
+            @endforeach
+        @endif
+    @else
+        {!! $meta !!}
+    @endif
 @endtypography
