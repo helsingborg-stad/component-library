5a3c9d8543b603407081ddbe3954ba4c8af3b3f9
Freya <752642+faejr@users.noreply.github.com>
2022-03-01 14:35:32 +0100

    fix: no links in meta

--- source/php/Component/Block/components/meta.blade.php
+++ source/php/Component/Block/components/meta.blade.php
@@ -1,16 +1,11 @@
 @typography(['variant' => 'meta', 'element' => 'span'])
-    @if(is_array($meta))
-        @if(array_filter($meta, 'is_string') !== [])
+    @if (is_array($meta))
+        @if (array_filter($meta, 'is_string') !== [])
             {{ implode(', ', $meta) }}
         @else
-            @foreach($meta as $value)
-                @if(isset($value['href']) && !empty($value['href']))
-                    @link(['href' => $value['href']])
-                        {!! $value['label'] !!}
-                    @endlink
-                @else
-                    {!! $value['label'] !!}
-                @endif
+            @foreach ($meta as $value)
+                {!! $value['label'] !!}
+                {{ !$loop->last ? ', ' : '' }}
             @endforeach
         @endif
     @else
