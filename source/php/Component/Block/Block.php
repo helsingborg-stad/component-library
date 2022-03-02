41b3dc14169ef4636640a1ffeb886617fe084849
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-02 09:22:22 +0100

    PSR-12 formatting and api for block

--- source/php/Component/Block/Block.php
+++ source/php/Component/Block/Block.php
@@ -8,28 +8,28 @@ namespace ComponentLibrary\Component\Block;
  */
 class Block extends \ComponentLibrary\Component\BaseController
 {
-	public function init()
-	{
-		// Extract array for easy access (fetch only)
-		extract($this->data);
+    public function init()
+    {
+        // Extract array for easy access (fetch only)
+        extract($this->data);
 
-		if ($image && !isset($image['backgroundColor'])) {
-			$this->data['image']['backgroundColor'] = 'white';
-		}
+        if ($image && !isset($image['backgroundColor'])) {
+            $this->data['image']['backgroundColor'] = 'white';
+        }
 
-		if ($image && isset($image['src'])) {
-			$this->data['attributeList']['style'] = 'background-image: url(' . $image['src'] . ')';
-		}
+        if ($image && isset($image['src'])) {
+            $this->data['attributeList']['style'] = 'background-image: url(' . $image['src'] . ')';
+        }
 
-		if ($link) {
-			$this->data['componentElement'] = "a";
-		} else {
-			$this->data['componentElement'] = "div";
-		}
+        if ($link) {
+            $this->data['componentElement'] = "a";
+        } else {
+            $this->data['componentElement'] = "div";
+        }
 
-		if ($ratio) {
-			$ratio = str_replace(":", "-", $ratio);
-			$this->data['classList'][] = $this->getBaseClass() . '--ratio-' . $ratio;
-		}
-	}
+        if ($ratio) {
+            $ratio = str_replace(":", "-", $ratio);
+            $this->data['classList'][] = $this->getBaseClass() . '--ratio-' . $ratio;
+        }
+    }
 }
