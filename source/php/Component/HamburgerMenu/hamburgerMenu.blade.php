58ba216754e85f862eda8c42b7f33adaffebb6ac
Erik Granström <erik.granstrom@helsingborg.se>
2022-03-16 14:19:54 +0100

    Change container class and padding

--- source/php/Component/HamburgerMenu/hamburgerMenu.blade.php
+++ source/php/Component/HamburgerMenu/hamburgerMenu.blade.php
@@ -1,33 +1,35 @@
 <!-- hamburgerMenu.blade.php -->
 <nav id="{{$id}}" class="{{$class}} u-padding__bottom--4" js-toggle-item="hamburger-menu" js-toggle-class="u-display--none">
-    <div class="container">
+    <div class="o-container">
         @if($showSearch)
-            @form(['method' => 'get', 'action' => '/'])
-                <label for="hamburger-menu-search" class="u-sr__only">{{ __('Search', 'component-library') }}</label>
+            <div class="u-padding__bottom--3">
+                @form(['method' => 'get', 'action' => '/'])
+                    <label for="hamburger-menu-search" class="u-sr__only">{{ __('Search', 'component-library') }}</label>
 
-                @group([])
-                    @field([
-                        'id' => 'hamburger-menu-search',
-                        'type' => 'text',
-                        'attributeList' => [
+                    @group([])
+                        @field([
+                            'id' => 'hamburger-menu-search',
                             'type' => 'text',
-                            'name' => 's',
-                        ],
-                        'placeholder' => __('What are you looking for?', 'component-library'),
-                        'required' => true,
-                    ])
-                    @endfield
-                    @button([
-                        'color' => 'primary',
-                        'type' => 'submit',
-                        'text' => __('Search', 'component-library')
-                    ])
-                    @endbutton
-                @endgroup
-            @endform
+                            'attributeList' => [
+                                'type' => 'text',
+                                'name' => 's',
+                            ],
+                            'placeholder' => __('What are you looking for?', 'component-library'),
+                            'required' => true,
+                        ])
+                        @endfield
+                        @button([
+                            'color' => 'primary',
+                            'type' => 'submit',
+                            'text' => __('Search', 'component-library')
+                        ])
+                        @endbutton
+                    @endgroup
+                @endform
+            </div>
         @endif
 
-        <ul class="{{$baseClass}}__list o-grid unlist u-padding__top--3">
+        <ul class="{{$baseClass}}__list o-grid unlist">
             @foreach ($menuItems as $item)
                 <li id="{{$id}}-item-{{$item['id']}}"
                     class="{{$baseClass}}__item {{$baseClass}}__item--parent o-grid-12 o-grid-6@md o-grid-4@lg u-mb-6 u-margin__top--1 {{$item['classNames']}}">
