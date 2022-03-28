b05e6dfe2bb0a15ad8a2dcfcf575cbacee65d568
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-03-28 10:13:03 +0200

    PSR fixes & add classes to datebadge

--- source/php/Component/Datebadge/Datebadge.php
+++ source/php/Component/Datebadge/Datebadge.php
@@ -8,5 +8,17 @@ class Datebadge extends \ComponentLibrary\Component\BaseController
     {
         //Extract array for eazy access (fetch only)
         extract($this->data);
+
+        //Sizes
+        if (in_array($size, ['sm', 'md'])) {
+            $this->data['classList'][] = $this->getBaseClass() . "--" . $size;
+        } else {
+            $this->data['classList'][] = $this->getBaseClass() . "--md";
+        }
+
+        //Format
+        $date = strtotime($date);
+        $this->data['month']    = date("M", $date);
+        $this->data['day']      = date("d", $date);
     }
 }
--- source/php/Component/Datebadge/datebadge.blade.php
+++ source/php/Component/Datebadge/datebadge.blade.php
@@ -1,9 +1,9 @@
 <!-- datebadge.blade.php -->
 <div id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
-    @typography(['variant' => 'h1', 'element' => 'span'])
+    @typography(['variant' => 'h1', 'element' => 'span', 'classList' => [$baseClass . '__date']])
         {{ $day }}
     @endtypography
-    @typography(['variant' => 'h4', 'element' => 'span'])
+    @typography(['variant' => 'h4', 'element' => 'span', 'classList' => [$baseClass . '__month']])
         {{ $month }}
     @endtypography
 </div>
