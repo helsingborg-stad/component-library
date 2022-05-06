88f21c517b4bede409ab0fdb1322147d34f45bea
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-05-06 15:41:30 +0200

    Initial box

---
+++ source/php/Component/Box/Box.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ComponentLibrary\Component\Box;
+
+/**
+ * Class Box
+ * @package ComponentLibrary\Component\Box
+ */
+class Box extends \ComponentLibrary\Component\BaseController
+{
+    public function init()
+    {
+        // Extract array for easy access (fetch only)
+        extract($this->data);
+
+        if ($link) {
+            $this->data['componentElement'] = "a";
+        } else {
+            $this->data['componentElement'] = "div";
+        }
+
+        if (!in_array($ratio, ['1:1', '4:3', '12:16'])) {
+            $ratio = '4:3';
+        }
+
+        if ($content) {
+            $this->data['content'] = $this->strWordCut(
+                strip_tags($content),
+                200
+            );
+        }
+
+        $this->data['classList'][] = $this->getBaseClass() . '--ratio-' . str_replace(":", "-", $ratio);
+    }
+
+    /**
+     * Create a excerpt from a string
+     *
+     * @param string $string
+     * @param int $length
+     * @param string $end
+     * @return string
+     */
+    private function strWordCut($string, $length, $end = '...')
+    {
+        $string = strip_tags($string);
+
+        if (strlen($string) > $length) {
+            $stringCut = substr($string, 0, $length);
+            $string = substr(
+                $stringCut,
+                0,
+                strrpos($stringCut, ' ')
+            ) . $end;
+        }
+
+        return $string;
+    }
+}
---
+++ source/php/Component/Box/box.blade.php
@@ -0,0 +1,67 @@
+<!-- block.blade.php -->
+<{{ $componentElement }} href="{{ $link }}" id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
+
+    @if($date && $dateBadge)
+        @datebadge(['date' => $date, 'classList' => ['u-position--absolute', 'u-margin--3', 'u-fixed--top-left']])
+        @enddatebadge
+    @endif
+
+    @if(!$slotHasData)
+        <div class="{{$baseClass}}__body">
+
+            @if($date && !$dateBadge)
+                @tags([
+                    'tags' => [
+                        ['label' => $date]
+                    ],
+                    'beforeLabel' => '',
+                    'format' => false
+                ])
+                @endtags
+            @endif
+
+            @if($meta)
+                @typography(['variant' => 'meta', 'element' => 'span', 'classList' => [$baseClass."__meta"]])
+                    @if (is_array($meta))
+                        @if (array_filter($meta, 'is_string') !== [])
+                            {{ implode(', ', $meta) }}
+                        @else
+                            @foreach ($meta as $value)
+                                {!! $value['label'] !!}
+                                {{ !$loop->last ? ', ' : '' }}
+                            @endforeach
+                        @endif
+                    @else
+                        {!! $meta !!}
+                    @endif
+                @endtypography
+            @endif
+
+            @if($heading)
+                @typography([
+                    'element'   => 'h2',
+                    'variant'   => 'h2',
+                    'classList' => [
+                        $baseClass."__heading"
+                    ]
+                ])
+                    {!! $heading !!}
+                @endtypography
+            @endif
+
+            @if($content)
+                @typography([
+                    'element'   => 'p',
+                    'variant'   => 'p',
+                    'classList' => [
+                        $baseClass."__content"
+                    ]
+                ])
+                    {!! $content !!}
+                @endtypography
+            @endif
+
+        </div>
+    @endif
+    {!! $slot !!}
+</{{ $componentElement }}>
