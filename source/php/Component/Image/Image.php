32d175ebf784a0e5aa16988b1c301cfac3080993
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2022-05-12 16:32:20 +0200

    Feature/box (#80)
    * Initial box
    
    * Let image component tell what fileextenstion beig used
    
    * Add box component
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Component/Image/Image.php
+++ source/php/Component/Image/Image.php
@@ -5,59 +5,64 @@ namespace ComponentLibrary\Component\Image;
 class Image extends \ComponentLibrary\Component\BaseController
 {
 
-	public function init()
-	{
+    public function init()
+    {
 
-		//Extract array for eazy access (fetch only)
-		extract($this->data);
+        //Extract array for eazy access (fetch only)
+        extract($this->data);
 
-		//Add placeholder class
-		if (!$src) {
-			$this->data['classList'][] = $this->getBaseClass() . "--is-placeholder";
-		}
-
-		//Make full width
-		if ($fullWidth) {
-			$this->data['classList'][] = $this->getBaseClass() . "--full-width";
-		}
-
-		//Inherit the alt text
-		if (!$alt && $caption) {
-			$this->data['alt'] = $this->data['caption'];
-		}
-
-		//Rounded corners all sides
-		if ($rounded) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded ";
-		}
-
-		//Rounded corners top left
-		if ($roundedTopLeft) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-left ";
-		}
-
-		//Rounded corners top right
-		if ($roundedTopRight) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-right ";
-		}
-
-		//Rounded corners bottom left
-		if ($roundedBottomLeft) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-left ";
-		}
-
-		//Rounded corners bottom right
-		if ($roundedBottomRight) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-right ";
-		}
-
-		//Rounded corners radius
-		if ($roundedRadius) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-".$roundedRadius;
+        //Add placeholder class
+        if (!$src) {
+            $this->data['classList'][] = $this->getBaseClass() . "--is-placeholder";
         }
-        
+
+        //Filetype
+        if ($src && $extension = pathinfo($src, PATHINFO_EXTENSION)) {
+            $this->data['classList'][] = $this->getBaseClass() . "--type-" . $extension;
+        }
+
+        //Make full width
+        if ($fullWidth) {
+            $this->data['classList'][] = $this->getBaseClass() . "--full-width";
+        }
+
+        //Inherit the alt text
+        if (!$alt && $caption) {
+            $this->data['alt'] = $this->data['caption'];
+        }
+
+        //Rounded corners all sides
+        if ($rounded) {
+            $this->data['classList'][] = $this->getBaseClass() . "--rounded ";
+        }
+
+        //Rounded corners top left
+        if ($roundedTopLeft) {
+            $this->data['classList'][] = $this->getBaseClass() . "--rounded-top-left ";
+        }
+
+        //Rounded corners top right
+        if ($roundedTopRight) {
+            $this->data['classList'][] = $this->getBaseClass() . "--rounded-top-right ";
+        }
+
+        //Rounded corners bottom left
+        if ($roundedBottomLeft) {
+            $this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-left ";
+        }
+
+        //Rounded corners bottom right
+        if ($roundedBottomRight) {
+            $this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-right ";
+        }
+
+        //Rounded corners radius
+        if ($roundedRadius) {
+                $this->data['classList'][] = $this->getBaseClass() . "--rounded-" . $roundedRadius;
+        }
+
         if ($openModal) {
-            $this->data['modalId'] = uniqid();
+                $this->data['modalId'] = uniqid();
         }
-	}
+    }
 }
