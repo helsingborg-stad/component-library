6159d75942430fa6adb237abe2dfa4234bde720d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-05-25 12:06:58 +0200

    Correct markup for comments sections

--- source/php/Component/Comment/comment.blade.php
+++ source/php/Component/Comment/comment.blade.php
@@ -1,60 +1,64 @@
 <{{$componentElement}} id="{{ $id }}" class="{{ $class }}"  {!! $attribute !!}>
-    <div class="{{$baseClass}}__top">
 
-        @link(['href' => $author_url, 'classList' => ['c-comment__link']])
-            @typography([
-                "variant" => "title",
-                "element" => "h6",
-                "classList" => [$baseClass.'__author']
-            ])
-                {{$author}}
-            @endtypography
-        @endlink
-
-        @if ($date)
-            @typography([
-                "variant" => "meta",
-                "element" => "p",
-                "classList" => [$baseClass.'__date']
-            ])
-                @date([
-                    'action' => 'timesince',
-                    'timestamp' => $date
-                ])
-                @enddate
-
-                @if ($date_suffix) {{$date_suffix}} @endif
-            @endtypography
-        @endif
-    </div>
-
-    <div class="{{$baseClass}}__bubble {{$baseClass}}__bubble--color-{{$bubble_color}}">
+    <div class="{{$baseClass}}__col">
         @avatar([
             'name' => $author,
             'image' => $author_image,
             'icon' => [
                 'name' => $icon,
                 'size' => 'lg'
-            ],
-            
+            ]
         ])
         @endavatar
+    </div>
 
-        @if($text)
-            @typography([
-                "variant" => "body",
-                "element" => "p",
-                "classList" => [$baseClass.'__bubble--inner']
-            ])
-                {!! $text !!}
-            @endtypography
-        @endif
+    <div class="{{$baseClass}}__col">
 
-        @if($slotHasData)
-            <div class="{{$baseClass}}__bubble--inner">
-                {!! $slot !!}
-            </div>
-        @endif
+        <div class="{{$baseClass}}__top">
 
+            @link(['href' => $author_url, 'classList' => ['c-comment__link']])
+                @typography([
+                    "variant" => "title",
+                    "element" => "h6",
+                    "classList" => [$baseClass.'__author']
+                ])
+                    {{$author}}
+                @endtypography
+            @endlink
+    
+            @if ($date)
+                @typography([
+                    "variant" => "meta",
+                    "element" => "span",
+                    "classList" => [$baseClass.'__date']
+                ])
+                    @date([
+                        'action' => 'timesince',
+                        'timestamp' => $date
+                    ])
+                    @enddate
+    
+                    @if ($date_suffix) {{$date_suffix}} @endif
+                @endtypography
+            @endif
+        </div>
+    
+        <div class="{{$baseClass}}__bubble {{$baseClass}}__bubble--color-{{$bubble_color}}">
+            @if($text)
+                @typography([
+                    "variant" => "body",
+                    "element" => "p",
+                    "classList" => [$baseClass.'__bubble--inner']
+                ])
+                    {!! $text !!}
+                @endtypography
+            @endif
+    
+            @if($slotHasData)
+                <div class="{{$baseClass}}__bubble--inner">
+                    {!! $slot !!}
+                </div>
+            @endif
+        </div>
     </div>
 </{{$componentElement}}>
