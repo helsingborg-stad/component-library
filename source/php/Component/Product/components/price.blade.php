983ba44ddffd05cad2969038f6735205fd3b155a
Freya <752642+faejr@users.noreply.github.com>
2022-06-10 14:30:34 +0200

    Fix from review

--- source/php/Component/Product/components/price.blade.php
+++ source/php/Component/Product/components/price.blade.php
@@ -1,34 +1,29 @@
-@foreach($prices as $price)
+@foreach ($prices as $price)
     <div class="{{ $baseClass }}__price {{ $baseClass }}__price--{{ $price['backgroundColor'] ?? 'primary' }}">
         @typography([
             'element' => 'span',
             'variant' => 'title',
-            'classList' => [
-                $baseClass . '__amount'
-            ]
+            'classList' => [$baseClass . '__amount']
         ])
             {{ $price['amount'] }}
         @endtypography
-        @if(!empty($price['currency']))
+        @if (!empty($price['currency']))
             @typography([
                 'element' => 'span',
                 'variant' => 'subtitle',
-                'classList' => [
-                    $baseClass . '__currency'
-                ]
+                'classList' => [$baseClass . '__currency']
             ])
                 {{ $price['currency'] }}
             @endtypography
         @endif
-        @if(!empty($price['frequency']))
+        @if (!empty($price['frequency']))
             @typography([
                 'element' => 'span',
                 'variant' => 'meta',
-                'classList' => [
-                    $baseClass . '__frequency'
-                ]
+                'classList' => [$baseClass . '__frequency']
             ])
-                /{{ $price['frequency'] }}
+                <span class="{{ $baseClass . '__seperator' }}">/</span>
+                {{ $price['frequency'] }}
             @endtypography
         @endif
     </div>
--- source/php/Component/Product/product.blade.php
+++ source/php/Component/Product/product.blade.php
@@ -1,32 +1,41 @@
 <!-- product.blade.php -->
-<div class="{{$class}}" id="{{ $id }}">
+<div class="{{ $class }}" id="{{ $id }}">
     @card()
         <div class="c-card__header">
             @typography([
-                'element'   => 'h2',
-                'variant'   => 'h3',
-                'classList' => [
-                    $baseClass . '__heading'
-                ]
+                'element' => 'h2',
+                'variant' => 'h3',
+                'classList' => [$baseClass . '__heading']
             ])
                 {{ $heading }}
             @endtypography
-            @typography()
+            @typography([
+                'element' => 'p',
+                'variant' => 'p',
+                'classList' => ['c-product__label']
+            ])
                 {{ $label }}
             @endtypography
             @includeWhen($prices, 'Product.components.price')
         </div>
         @includeWhen($image && $image['src'], 'Product.components.image')
         <div class="c-card__body">
-            @if($meta)
-                @typography()
+            @if ($meta)
+                @typography([
+                    'element' => 'p',
+                    'variant' => 'meta',
+                    'classList' => ['c-product__listing']
+                ])
                     {{ $meta }}
                 @endtypography
             @endif
-            @listing([
-                'list' => $bulletPoints
-            ])
-            @endlisting
+            @if (!empty($bulletPoints))
+                @listing([
+                    'list' => $bulletPoints,
+                    'classList' => ['c-product__listing']
+                ])
+                @endlisting
+            @endif
         </div>
         @includeWhen($button, 'Product.components.button')
     @endcard
