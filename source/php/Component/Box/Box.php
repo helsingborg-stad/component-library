5fda983393a468af5f6550ed3fc9e253e761ed82
ergr1001 <97021637+ergr1001@users.noreply.github.com>
2022-06-14 18:39:19 +0200

    feat: Add support for icon component (#93)

--- source/php/Component/Box/Box.php
+++ source/php/Component/Box/Box.php
@@ -30,8 +30,31 @@ class Box extends \ComponentLibrary\Component\BaseController
             );
         }
 
+        //Reset - Decides how to switch between data inputs
+		$this->renderMostImportant();
+
         $this->data['classList'][] = $this->getBaseClass() . '--ratio-' . str_replace(":", "-", $ratio);
     }
+
+    /**
+	 * renderMostImportant
+	 */
+	public function renderMostImportant()
+	{
+		//Reset icon if image set
+		if ($this->data['image']['src'] ?? false) {
+			$this->data['icon'] = null;
+		} else {
+            $this->data['image'] = null;
+        }
+
+		//Reset image if icon set
+		if ($this->data['icon']['name'] ?? false) {
+			$this->data['image'] = null;
+		} else {
+            $this->data['icon'] = null;
+        }
+	}
 
     /**
      * Create a excerpt from a string
