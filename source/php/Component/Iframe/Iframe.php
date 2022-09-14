819a1991d52144634cd692f2829e13e8caf5eb58
Anna <anna@thehappyhipster.se>
2022-09-14 05:56:56 +0200

    Documentation for the iframe component

--- source/php/Component/Iframe/Iframe.php
+++ source/php/Component/Iframe/Iframe.php
@@ -9,5 +9,19 @@ class Iframe extends \ComponentLibrary\Component\BaseController
 
         extract($this->data);
 
+		if ($src) {
+			$this->data['attributeList']['src'] = $src;
+			$this->data['attributeList']['data-src'] = $src;
+		}
+		if ($title) {
+			$this->data['attributeList']['title'] = $title;
+		}
+		if ($width) {
+			$this->data['attributeList']['width'] = $width;
+		}
+		if ($height) {
+			$this->data['attributeList']['height'] = $height;
+		}
+
     }
 }
--- source/php/Component/Iframe/iframe.blade.php
+++ source/php/Component/Iframe/iframe.blade.php
@@ -1,12 +1,6 @@
 <!-- iframe.blade.php -->
 @if($src)
-<iframe id="{{ $id }}" class="{{ $class }}" width="{{$width}}" height="{{$height}}" data-src={{$rc}} src={{$rc}}>
-	@if($errorMessage)
-		@notice(['isWarning' => true])
-		{{$errorMessage}}
-		@endnotice
-	@endif
-</iframe>
+<iframe id="{{ $id }}" class="{{ $class }}" src="{{ $src }}" width="{{ $width }}" height="{{ $height }}" {!! $attribute !!}></iframe>
 @else 
 <!-- No iframe source defined -->
 @endif
