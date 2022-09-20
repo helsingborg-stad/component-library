5a1309a1151e89a40e1f13ef7ebbcffda5ceb854
Anna Johansson <anna@thehappyhipster.se>
2022-09-20 09:28:11 +0200

    Adding iframe component

--- source/php/Component/Iframe/Iframe.php
+++ source/php/Component/Iframe/Iframe.php
@@ -2,15 +2,63 @@
 
 namespace ComponentLibrary\Component\Iframe;
 
-class Iframe extends \ComponentLibrary\Component\BaseController  
+class Iframe extends \ComponentLibrary\Component\BaseController
 {
-    
-    public function init() {
-
+    public function init()
+    {
         extract($this->data);
-		
-		if($src) {
+
+        if ($src) {
             $this->data['attributeList']['data-src'] = $src;
+			$this->data['attributeList']['data-supplier'] = '$src';
+
+            $suppliers = $this->getSuppliers();
+            if (is_array($suppliers)) {
+                $src_url = parse_url($src);
+
+                foreach ($suppliers as $supplier) {
+                    foreach ($supplier->domains as $domain) {
+                        if ($src_url['host'] == $domain_url) {
+                            $this->data['attributeList']['data-supplier'] = $domain_url;
+                        }
+                    }
+                }
+            }
         }
-	}
+    }
+    public function getSuppliers()
+    {
+        $suppliers = array(
+            new Supplier(
+                'Google Maps',
+                array( 'maps.google.com', 'google.com/maps', 'www.google.com/maps'),
+                true,
+                'https://policies.google.com/privacy?hl=en-US'
+            ),
+            new Supplier(
+                'YouTube',
+                array( 'youtube.com', 'www.youtube.com' ),
+                true,
+                'https://policies.google.com/privacy?hl=en-US'
+            ),
+            new Supplier(
+                'Helsingborg Stad',
+                array( 'helsingborg.se', 'www.helsingborg.se'),
+                true,
+            ),
+        );
+
+        return $suppliers;
+    }
+}
+
+class Supplier
+{
+    public function __construct(string $name, array $domains, bool $requires_accept = true, string $policy_document = '')
+    {
+        $this->name = $name;
+        $this->domains = $domains;
+        $this->requires_accept = $requires_accept;
+        $this->policy_document = $policy_document;
+    }
 }
