089dd2cf8173351a5f6a884d030dc62ceb645bdb
Anna Johansson <anna@thehappyhipster.se>
2022-09-22 15:37:14 +0200

    Setting up defaults

--- source/php/Component/Iframe/Iframe.php
+++ source/php/Component/Iframe/Iframe.php
@@ -4,68 +4,84 @@ namespace ComponentLibrary\Component\Iframe;
 
 class Iframe extends \ComponentLibrary\Component\BaseController
 {
-	public function init()
-	{
-		extract($this->data);
+    public function init()
+    {
+        extract($this->data);
 
-		if ( $options ) {
-			$this->data['attributeList']['options'] = $options;
-		}
+        if ($width) {
+            $this->data['attributeList']['width'] = $width;
+        }
 
-		if ($src) {
-			$this->data['attributeList']['data-src'] = $src;
+        if ($height) {
+            $this->data['attributeList']['height'] = $height;
+        }
 
-			$suppliers = $this->getSuppliers();
+        if ($frameborder) {
+            $this->data['attributeList']['frameborder'] = $frameborder;
+        }
 
-			if (is_array($suppliers)) {
-				$src_parsed = parse_url($src);
+        if ($options) {
+            $this->data['attributeList']['options'] = $options;
+        }
+        if ($loading) {
+            $this->data['attributeList']['loading'] = $loading;
+        }
 
-				foreach ($suppliers as $supplier) {
-	
-					$key = array_search($src_parsed['host'], $supplier->domain, true);
-	
-					if (is_integer($key)) {
-						$this->data['attributeList']['data-supplier-host'] = $supplier->domain[$key];
-						$this->data['attributeList']['data-supplier-name'] = $supplier->name;
-						if (isset($supplier->policy)) {
-							$this->data['attributeList']['data-supplier-policy'] = $supplier->policy;
-						}
-					}
-				}
-			}
-		}
-	}
-	public function getSuppliers()
-	{
-		$suppliers = array(
-			new Supplier(
-				'Google',
-				array( 'google.com', 'maps.google.com', 'google.se', 'maps.google.se' ),
-				'https://policies.google.com/privacy'
-			),
-			new Supplier(
-				'YouTube',
-				array( 'youtube.com', 'www.youtube.com', 'youtu.be' ),
-				'https://policies.google.com/privacy'
-			),
-			new Supplier(
-				'Vimeo',
-				array( 'vimeo.com', 'www.vimeo.com' ),
-				'https://policies.google.com/privacy'
-			),
-		);
+        $this->data['attributeList']['src'] = "about:blank";
 
-		return $suppliers;
-	}
+        if ($src) {
+            $this->data['attributeList']['data-src'] = $src;
+
+            $suppliers = $this->getSuppliers();
+
+            if (is_array($suppliers)) {
+                $src_parsed = parse_url($src);
+
+                foreach ($suppliers as $supplier) {
+                    $key = array_search($src_parsed['host'], $supplier->domain, true);
+
+                    if (is_integer($key)) {
+                        $this->data['attributeList']['data-supplier-host'] = $supplier->domain[$key];
+                        $this->data['attributeList']['data-supplier-name'] = $supplier->name;
+                        if (isset($supplier->policy)) {
+                            $this->data['attributeList']['data-supplier-policy'] = $supplier->policy;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public function getSuppliers()
+    {
+        $suppliers = array(
+            new Supplier(
+                'Google',
+                array( 'google.com', 'maps.google.com', 'google.se', 'maps.google.se' ),
+                'https://policies.google.com/privacy'
+            ),
+            new Supplier(
+                'YouTube',
+                array( 'youtube.com', 'www.youtube.com', 'youtu.be' ),
+                'https://policies.google.com/privacy'
+            ),
+            new Supplier(
+                'Vimeo',
+                array( 'vimeo.com', 'www.vimeo.com' ),
+                'https://policies.google.com/privacy'
+            ),
+        );
+
+        return $suppliers;
+    }
 }
 
 class Supplier
 {
-	public function __construct(string $name, array $domain, string $policy = '', bool $requires_accept = true)
-	{
-		$this->name = $name;
-		$this->domain = $domain;
-		$this->policy = $policy;
-		$this->requires_accept = $requires_accept;
-	}
+    public function __construct(string $name, array $domain, string $policy = '', bool $requires_accept = true)
+    {
+        $this->name = $name;
+        $this->domain = $domain;
+        $this->policy = $policy;
+        $this->requires_accept = $requires_accept;
+    }
 }
--- source/php/Component/Iframe/iframe.blade.php
+++ source/php/Component/Iframe/iframe.blade.php
@@ -2,11 +2,5 @@
 <iframe 
     id="{{ $id }}" 
     class="{{ $class }}" 
-    data-src="{{ $src }}" 
-    loading="{{ $loading }}" 
-    height="{{ $height }}" 
-    width="{{ $width }}"
-    frameborder="0" 
-    src="{{ $src }}"
 	{!! $attribute !!}>
 </iframe>
