53e3d78a25b7f77bbc96648b1baf9153cac655c8
Cedrik <cedrikvonheiroth@gmail.com>
2022-10-03 14:29:58 +0200

    hard code psedou coding

--- source/php/Component/Testimonials/testimonials.blade.php
+++ source/php/Component/Testimonials/testimonials.blade.php
@@ -14,13 +14,58 @@
 @endif
 
 @if (count($testimonials) > 1 && $isCarousel)
-    @slider([])
-        @foreach($testimonials as $testimonial)
+    @slider([
+        'showStepper' => true,
+        'autoSlide' => false,
+        'repeatSlide' => true,
+    ])
+        @slider__item([
+            'title' => 'First slider item',
+            'sub_title' => 'This is a slightly longer title called subtitle',
+            'text' => 'Here I could put some lorem ipsum text but I am too lazy to Google for one so I wrote all this instead. Woo!',
+            'layout' => 'center',
+            'containerColor' => 'none',
+            'overlay' => 'dark',
+            'textColor' => 'white',
+            'textAlignment' => 'center',
+            'mobile_image' => 'https://picsum.photos/720/720',
+            'desktop_image' => 'https://picsum.photos/1080/720',
+            'heroStyle' => true
+        ])
+        @endslider__item
+
+        @slider__item([
+        ])
             @card([
-                'classList' => [$class]
-            ])
-                @include('Testimonials.partials.item')
-            @endcard
-        @endforeach
+            ])                
+                <div class="{{ $baseClass}}__header">
+                    @typography([
+                            "element" => "h2",
+                            "classList" => ['u-color__text--darker']
+                        ])
+                        name
+                    @endtypography
+                
+                    @typography([                            
+                        "element" => "h3",
+                        'variant' => 'h3',
+                        "classList" => ['u-color__text--darker']
+                    ])
+                        title
+                    @endtypography
+                </div>
+                
+                <div class="{{ $baseClass }}__quote">
+                    @typography([
+                        "variant" => "p",
+                        "element" => "p",
+                        "classList" => ['u-color__text--darker']
+                    ])
+                        "testimonial..."
+                    @endtypography
+                </div>
+            @endcard 
+        @endslider__item
+
     @endslider
 @endif
