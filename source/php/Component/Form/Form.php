c537e5cec7c2809b581eabaf1dfd71009e6d057b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-02 11:56:53 +0100

    Add default unique id, add js form validation class.

--- source/php/Component/Form/Form.php
+++ source/php/Component/Form/Form.php
@@ -11,5 +11,16 @@ class Form extends \ComponentLibrary\Component\BaseController
     public function init()
     {
         extract($this->data);
+
+        //Id always required
+        if (!$id) {
+            $this->data['id'] = uniqid();
+        }
+
+        //Add validation class as default
+        if ($validation) {
+            $this->data['classList'][] = 'js-form-validation';
+        }
+
     }
 }
