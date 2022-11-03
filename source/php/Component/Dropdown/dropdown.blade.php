a49a8608f4e57af12a21d993b79769fa1d901efa
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-11-03 15:11:57 +0100

    add back attribute on root element & set aria-label on a element

--- source/php/Component/Dropdown/dropdown.blade.php
+++ source/php/Component/Dropdown/dropdown.blade.php
@@ -1,11 +1,12 @@
-<{{ $componentElement }} id="{{ $id }}" class="{{ $class }}">
+<{{ $componentElement }} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
     {{ $slot }}
-    <div class="{{ $baseClass }}__list {{ $baseClass }}__list--{{ $direction }}">
+    <div class="{{ $baseClass }}__list
+    {{ $baseClass }}__list--{{ $direction }}">
         <div>
             <ul class="u-margin__top--0">
                 @foreach ($items as $item)
                     <li @if (!empty($item['classList'])) class="{{ implode(' ', $item['classList']) }}" @endif>
-                        <{{ $itemElement }} href="{{ $item['link'] }}">
+                        <{{ $itemElement }} href="{{ $item['link'] }}" aria-label="{{ $item['text'] }}">
                             {{ $item['text'] }}
                             </{{ $itemElement }}>
                     </li>
