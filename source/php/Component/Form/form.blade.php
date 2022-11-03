bea8ad4d633494158cb0ca79be41ef900675f9a3
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2022-11-03 15:31:44 +0100

    Fix/form id attribute move (#153)
    * Remove comp params, implement automatic detection of attributes / field attributes
    
    * Clarify docs
    
    * Cleanup views
    
    * Remove codesmells
    
    * Add parameter warnings, fallback to attributes.
    
    * Fix date & documentation
    
    * Add some examples if textareas.
    
    * Add support for number of rows in multiline.
    
    Bool = textarea resizable.
    Number = texarea with n-lines, not resizable.
    
    * Add default unique id, add js form validation class.
    
    * Add notice components
    
    * Formatting
    
    * Add parameters for data validation / message
    
    * Remove title field, as it simplifies validation messages
    
    * Add regexp and message as disallowed attributes.
    
    * Prevent letter ”e” to be entered in number fields.
    
    * Prevent + from being used in email adresses
    
    * Add js-filter-input as field attribute
    
    * Update with parameters for validation and error messages on form
    
    Also hides them if no message is set.
    
    * Reverse check of is field attribute.
    
    List what should be placed in component level. Not the other way around.
    
    * Fix broken checks for type
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Component/Form/form.blade.php
+++ source/php/Component/Form/form.blade.php
@@ -1,4 +1,53 @@
 <!-- form.blade.php -->
 <form class="{{$class}}" {!! $attribute !!} method="{{$method}}" action="{{$action}}">
+
+    @if($errorMessage)
+        @notice([
+            'id' => $id . '-validation-error',
+            'type' => 'danger',
+            'message' => [
+                'text' => $errorMessage
+            ],
+            'icon' => [
+                'name' => 'report',
+                'size' => 'md',
+                'color' => 'black'
+            ],
+            'classList' => [
+                $baseClass . '__notice-failed',
+                'u-margin__bottom--4',
+                'u-display--none'
+            ],
+            'attributeList' => [
+                'aria-hidden' => 'true',
+            ]
+        ])
+        @endnotice
+    @endif
+
+    @if($validateMessage)
+        @notice([
+            'id' => $id . '-validation-sucess',
+            'type' => 'success',
+            'message' => [
+                'text' => $validateMessage,
+            ],
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'black'
+            ],
+            'classList' => [
+                $baseClass . '__notice-success',
+                'u-margin__bottom--4',
+                'u-display--none'
+            ],
+            'attributeList' => [
+                'aria-hidden' => 'true',
+            ]
+        ])
+        @endnotice
+    @endif
+
     {!! $slot !!}
 </form>
