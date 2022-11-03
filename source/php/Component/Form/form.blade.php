d6c7fc001133cc792fc6fa17fc69cbb487a8c391
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-03 09:35:17 +0100

    Update with parameters for validation and error messages on form
    Also hides them if no message is set.

--- source/php/Component/Form/form.blade.php
+++ source/php/Component/Form/form.blade.php
@@ -1,49 +1,53 @@
 <!-- form.blade.php -->
 <form class="{{$class}}" {!! $attribute !!} method="{{$method}}" action="{{$action}}">
 
-    @notice([
-        'id' => $id . '-validation-error',
-        'type' => 'danger',
-        'message' => [
-            'text' => '@{{FORM_VALIDATION_ERROR_MESSAGE}}'
-        ],
-        'icon' => [
-            'name' => 'report',
-            'size' => 'md',
-            'color' => 'black'
-        ],
-        'classList' => [
-            $baseClass . '__notice-failed',
-            'u-margin__bottom--4',
-            //'u-display--none'
-        ],
-        'attributeList' => [
-            'aria-hidden' => 'true',
-        ]
-    ])
-    @endnotice
+    @if($errorMessage)
+        @notice([
+            'id' => $id . '-validation-error',
+            'type' => 'danger',
+            'message' => [
+                'text' => $errorMessage
+            ],
+            'icon' => [
+                'name' => 'report',
+                'size' => 'md',
+                'color' => 'black'
+            ],
+            'classList' => [
+                $baseClass . '__notice-failed',
+                'u-margin__bottom--4',
+                'u-display--none'
+            ],
+            'attributeList' => [
+                'aria-hidden' => 'true',
+            ]
+        ])
+        @endnotice
+    @endif
 
-    @notice([
-        'id' => $id . '-validation-sucess',
-        'type' => 'success',
-        'message' => [
-            'text' => '@{{FORM_VALIDATION_SUCCESS_MESSAGE}}',
-        ],
-        'icon' => [
-            'name' => 'check',
-            'size' => 'md',
-            'color' => 'black'
-        ],
-        'classList' => [
-            $baseClass . '__notice-success',
-            'u-margin__bottom--4',
-            //'u-display--none'
-        ],
-        'attributeList' => [
-            'aria-hidden' => 'true',
-        ]
-    ])
-    @endnotice
+    @if($validateMessage)
+        @notice([
+            'id' => $id . '-validation-sucess',
+            'type' => 'success',
+            'message' => [
+                'text' => $validateMessage,
+            ],
+            'icon' => [
+                'name' => 'check',
+                'size' => 'md',
+                'color' => 'black'
+            ],
+            'classList' => [
+                $baseClass . '__notice-success',
+                'u-margin__bottom--4',
+                'u-display--none'
+            ],
+            'attributeList' => [
+                'aria-hidden' => 'true',
+            ]
+        ])
+        @endnotice
+    @endif
 
     {!! $slot !!}
 </form>
