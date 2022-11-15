fdf29fdbd5dc02b6beb038cae78348cf32d6983c
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-15 11:54:20 +0100

    I only changed row 181 adding } and 162 adding implode()

--- source/php/Component/Acceptance/Acceptance.php
+++ source/php/Component/Acceptance/Acceptance.php
@@ -159,26 +159,26 @@ class Acceptance extends \ComponentLibrary\Component\BaseController
         } else {
     $suppliers  = $this->getSuppliers();
 
-        $srcParsed = parse_url($src);
-        $host = strtolower($srcParsed['host']);
+    $srcParsed = parse_url(implode($src));
+    $host = strtolower($srcParsed['host']);
 
-        if (is_iterable($suppliers)) {
-            foreach ($suppliers as $supplier) {
-                $key = array_search($host, $supplier->domain, true);
-                if (is_integer($key)) {
-                    $this->data['supplierHost'] = $supplier->domain[$key];
-                    $this->data['supplierName'] = $supplier->name;
-                    $this->data['requiresAccept'] = $supplier->requiresAccept;
+    if (is_iterable($suppliers)) {
+        foreach ($suppliers as $supplier) {
+            $key = array_search($host, $supplier->domain, true);
+            if (is_integer($key)) {
+                $this->data['supplierHost'] = $supplier->domain[$key];
+                $this->data['supplierName'] = $supplier->name;
+                $this->data['requiresAccept'] = $supplier->requiresAccept;
 
-                    if (isset($supplier->policy)) {
-                        $this->data['supplierPolicy'] = $supplier->policy;
-                    }
-                } else {
-                    $this->data['supplierHost'] = $host;
+                if (isset($supplier->policy)) {
+                    $this->data['supplierPolicy'] = $supplier->policy;
                 }
+            } else {
+                $this->data['supplierHost'] = $host;
             }
         }
-
+    }
+}
         return $this->data;
     }
 }
