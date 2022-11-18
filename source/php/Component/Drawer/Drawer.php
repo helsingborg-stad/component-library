545113e19c5b3c01b888910763b7fcc88265dd41
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-18 14:27:59 +0100

    This

--- source/php/Component/Drawer/Drawer.php
+++ source/php/Component/Drawer/Drawer.php
@@ -8,8 +8,9 @@ class Drawer extends \ComponentLibrary\Component\BaseController
     {
      //Extract array for eazy access (fetch only)
         extract($this->data);
-        $data['searchSlotHasData'] = !empty($this->accessProtected($this->data['search'], "html"));
-        $data['menuSlotHasData'] = !empty($this->accessProtected($this->data['menu'], "html"));
+
+        $this->data['searchSlotHasData'] = !empty($this->accessProtected($this->data['search'], "html"));
+        $this->data['menuSlotHasData'] = !empty($this->accessProtected($this->data['menu'], "html"));
         
     }
 }
