3aced455e8e7640791aa104706646bca83ae71ac
Anna Johansson <anna@thehappyhipster.se>
2022-11-18 14:30:10 +0100

    Always allow fullscreen mode on iframes

--- source/php/Component/Iframe/iframe.blade.php
+++ source/php/Component/Iframe/iframe.blade.php
@@ -5,6 +5,7 @@
     "height"    => $height,
     "src"       => array($attributeList['src']),
     "icon"      => "info",
+	"allowfullscreen" => false
     ])
     <iframe 
         id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
