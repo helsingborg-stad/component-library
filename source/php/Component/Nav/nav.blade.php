a77b271abde240330d1d6a820336ac04a5bf2d31
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-22 11:39:02 +0100

    Remove role=”menu” on nav
    This is not needed anymore.

--- source/php/Component/Nav/nav.blade.php
+++ source/php/Component/Nav/nav.blade.php
@@ -1,6 +1,6 @@
 <!-- nav.blade.php -->
 @if ($items) 
-    <ul class="{{$class}}" {!! $attribute !!} role="menu">
+    <ul class="{{$class}}" {!! $attribute !!}>
         @foreach ($items as $item)
             <li 
                 id="{{$id}}-{{$item['id']}}-{{$loop->index}}__item" class="{{$baseClass}}__item {{$baseClass}}__item--{{$item['style']}} {{$item['active'] ? 'is-current' : ''}}{{$item['active'] && $item['children'] || $item['ancestor'] ? ' is-open has-fetched' : ''}} {{!$item['active'] && is_array($item['children']) ? ' has-fetched' : ''}}"
