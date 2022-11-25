1b918ba09d22b9807e1bf198ecd7a7056fe76217
Cedrik <cedrikvonheiroth@gmail.com>
2022-11-25 09:54:33 +0100

    hero container

--- source/php/Component/Hero/hero.blade.php
+++ source/php/Component/Hero/hero.blade.php
@@ -6,40 +6,42 @@
             <div class="{{ $baseClass }}__overlay"></div>
         @endif
 
-        @if ($title || $paragraph || $byline)
-            <div class="o-container {{ $baseClass }}__container">
-                @if($video)
-                    <video autoplay muted loop class="c-hero__video">
-                        <source src="{{$video}}" type="video/mp4">
-                    </video>
-                @endif
-
-                <div class="{{ $baseClass }}__content">
-
-                    @if ($title)
-                        @typography(['variant' => 'h1', 'element' => 'h1', 'classList' => [$baseClass . '__title']])
-                            {!! $title !!}
-                        @endtypography
-                    @endif
-
-                    @if ($byline)
-                        @typography(['variant' => 'h2', 'element' => 'span', 'classList' => [$baseClass . '__byline']])
-                            {!! $byline !!}
-                        @endtypography
-                    @endif
-
-                    @if ($paragraph)
-                        @typography(['variant' => 'p', 'element' => 'p', 'classList' => [$baseClass . '__body']])
-                            {!! $paragraph !!}
-                        @endtypography
-                    @endif
-
-                    {{-- Oneline to enable the use of css:empty() function --}}
-                    <div class="{{ $baseClass }}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
-
-                </div>
-
-            </div>
-        @endif
     </section>
+
+    @if ($title || $paragraph || $byline)
+        <div class="o-container {{ $baseClass }}__container">
+            @if($video)
+                <video autoplay muted loop class="c-hero__video">
+                    <source src="{{$video}}" type="video/mp4">
+                </video>
+            @endif
+
+            
+            @endif
+            
+            <div class="{{ $baseClass }}__content">
+                
+                @if ($title)
+                @typography(['variant' => 'h1', 'element' => 'h1', 'classList' => [$baseClass . '__title']])
+                {!! $title !!}
+                @endtypography
+                @endif
+                
+                @if ($byline)
+                @typography(['variant' => 'h2', 'element' => 'span', 'classList' => [$baseClass . '__byline']])
+                {!! $byline !!}
+                @endtypography
+                @endif
+                
+                @if ($paragraph)
+                @typography(['variant' => 'p', 'element' => 'p', 'classList' => [$baseClass . '__body']])
+                {!! $paragraph !!}
+                @endtypography
+                @endif
+                
+                {{-- Oneline to enable the use of css:empty() function --}}
+                <div class="{{ $baseClass }}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
+                
+            </div>
+        </div>
 </div> 
