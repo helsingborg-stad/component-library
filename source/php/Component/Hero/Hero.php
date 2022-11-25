376b5369ac2d592a4c46ceb7ca0a432cbc63f7cd
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-25 11:55:33 +0100

    Adds class "--has-animation" if animation is selected

--- source/php/Component/Hero/Hero.php
+++ source/php/Component/Hero/Hero.php
@@ -54,5 +54,11 @@ class Hero extends \ComponentLibrary\Component\BaseController
         } else {
             $this->data['overlay'] = false;
         }
+
+        if(isset($sidebar) && $sidebar == 'sidebar.slider-area') {
+            if(empty(get_theme_mod('hero_animation'))) {
+                $this->data['classList'][] = $this->getBaseClass() . '--has-animation';
+            }
+        }
     }
 }
