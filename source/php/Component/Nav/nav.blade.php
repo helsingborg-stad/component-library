1280aa177a030d8387fb981ce2225856460bbcfe
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-11-25 15:21:26 +0100

    Do not render text label if empty

--- source/php/Component/Nav/nav.blade.php
+++ source/php/Component/Nav/nav.blade.php
@@ -30,13 +30,15 @@
                     <a  id="{{$id}}-{{$item['id']}}-{{$loop->index}}__label"
                         class="{{$baseClass}}__link {{$item['class']}}" 
                         href="{{$item['href']}}" 
-                        aria-label="{{$item['label']}}" 
+                        @if($item['label']) aria-label="{{$item['label']}}" @endif
                     >
                         @if(isset($item['icon']))
                             @icon($item['icon'])
                             @endicon
                         @endif
-                        <span class="{{$baseClass}}__text">{{$item['label']}}</span>
+                        @if($item['label'])
+                            <span class="{{$baseClass}}__text">{{$item['label']}}</span>
+                        @endif
                     </a>
                 @endif
                 
