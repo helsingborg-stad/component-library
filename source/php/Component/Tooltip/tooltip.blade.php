aab40c9dc076c9724d00607f434d55ca0289efa8
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-08 15:57:40 +0100

    init

--- source/php/Component/Tooltip/tooltip.blade.php
+++ source/php/Component/Tooltip/tooltip.blade.php
@@ -1,4 +1,4 @@
-<{{ $componentElement }} 
+{{-- <{{ $componentElement }} 
     @if($isLink)
         href="{{ $href }}" 
     @endif
@@ -6,4 +6,23 @@
     {!! $attribute !!}
     >
     {{$beforeContent}} {{ $slot }} {{$afterContent}}
-</{{ $componentElement }}>
+</{{ $componentElement }}> --}}
+
+<div class="{{$class}}">
+    @if($label) {
+        {{$label}}
+    }
+    @endif
+    @if($icon)
+        @icon([
+            'icon' => 'arrow_circle_up',
+            'size' => 'md',
+        ])
+        @endicon
+    @endif
+    <div class="{{$baseClass}}__container" style="background-color: {{$backgroundColor}}; color:{{$backgroundColor}}; width:{{$width}}">
+        <div class="{{$baseClass}}__content" style="color: {{$color}}">TJA! TJA! TJA! TJA!TJA! TJA!TJA! TJA!TJA!TJA!TJA!TJA!TJA!</div>
+        <span class="{{$baseClass}}__arrow"></span>
+    </div>
+
+</div>
