e673540decf73684403760bdd192598f94ddd550
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-14 17:19:38 +0100

    Progressbar component

---
+++ source/php/Component/Progress/Progress.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ComponentLibrary\Component\Progress;
+
+class Progress extends \ComponentLibrary\Component\BaseController  
+{
+    
+    public function init() {
+
+        //Extract array for easy access (fetch only)
+        extract($this->data);
+     
+        
+    }
+}
---
+++ source/php/Component/Progress/progress.blade.php
@@ -0,0 +1,18 @@
+<div class="{{$class}}" {!! $attribute !!}>
+{{--     @group([
+
+    ])
+        @typography([
+            'element' => 'strong',
+        ])
+        Hej
+        @endtypography
+        @tooltip([
+
+        ])
+        @endtooltip
+    @endgroup --}}
+    <div class="{{$baseClass}}__bar">
+        <div class="{{$baseClass}}__value"></div>
+    </div>
+</div>
