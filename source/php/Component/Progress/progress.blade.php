d665cc04919502763d62ec27ebce1f139fe68a57
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-15 08:01:55 +0100

    fix

--- source/php/Component/Progress/progress.blade.php
+++ source/php/Component/Progress/progress.blade.php
@@ -1,17 +1,4 @@
 <div class="{{$class}}" {!! $attribute !!}>
-{{--     @group([
-
-    ])
-        @typography([
-            'element' => 'strong',
-        ])
-        Hej
-        @endtypography
-        @tooltip([
-
-        ])
-        @endtooltip
-    @endgroup --}}
     <div class="{{$baseClass}}__bar">
         <div class="{{$baseClass}}__value"></div>
     </div>
