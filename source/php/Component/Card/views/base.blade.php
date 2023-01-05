44f96f359cf8e780e107d1d9744f4ce0b65e32bc
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-01-05 12:51:45 +0100

    fix: undefined errors

--- source/php/Component/Card/views/base.blade.php
+++ source/php/Component/Card/views/base.blade.php
@@ -1,3 +1,3 @@
-@includeWhen($image && $image['src'], 'Card.components.image')
-@includeWhen($image && $image['src'] && $dateBadge && $date, 'Card.components.datebadge')
+@includeWhen($image && !empty($image['src']), 'Card.components.image')
+@includeWhen($image && !empty($image['src']) && $dateBadge && $date, 'Card.components.datebadge')
 @includeWhen($collapsible || $heading || $subHeading || $meta || ($date && !$dateBadge) || $content || $tags || $buttons, 'Card.partials.body')
