333ceed3eeabbe808c08a991d77492e6eb081b20
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-02-20 14:34:44 +0100

    Default view: Support unlabelled expand icons.

--- source/php/Component/Nav/style/default.blade.php
+++ source/php/Component/Nav/style/default.blade.php
@@ -1,8 +1,12 @@
-@link([
-  'id' => $id . '-' . $item['id'] . '-' . $loop->index . '__label',
-  'classList' => [$baseClass . '__link'],
-  'href' => $item['href'],
-])
-  @icon($item['icon'])@endicon
-  <span class="{{$baseClass}}__text">{{$item['label']}}</span>
-@endlink
+@if($item['icon'] || $item['label'])
+  @link([
+    'id' => $id . '-' . $item['id'] . '-' . $loop->index . '__label',
+    'classList' => [$baseClass . '__link'],
+    'href' => $item['href'],
+  ])
+    @icon($item['icon'])@endicon
+    <span class="{{$baseClass}}__text">{{$item['label']}}</span>
+  @endlink
+@else
+  <!-- Hidden link: Both label and icon is missing -->
+@endif
