196fdf9190722e1da2337caf53cee994a0598f06
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-02-20 15:33:34 +0100

    Render expand label

--- source/php/Component/Nav/children.blade.php
+++ source/php/Component/Nav/children.blade.php
@@ -5,6 +5,7 @@
       'icon' => 'expand_more',
       'size' => 'md',
       'pressed' => $item['active'] ? 'true' : 'false',
+      'aria-label' => ($item['label'] ? ($expandLabel . ': ' . $item['label']) : $expandLabel)
   ])
   @endbutton
 @endif
