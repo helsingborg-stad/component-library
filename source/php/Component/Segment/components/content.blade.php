2b69f645bc19e7dd7b2ab6cacf023558eedb5dce
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-24 10:30:29 +0100

    segment card view

--- source/php/Component/Segment/components/content.blade.php
+++ source/php/Component/Segment/components/content.blade.php
@@ -1,12 +1,28 @@
 @if($title)
     @typography([
         "element" => "h2",
-        "variant" => ($layout == 'full-width') ? 'h1' : 'h2',
+        "variant" => ($layout == 'full-width' || $layout == 'card') ? 'h1' : 'h2',
         "classList" => [$baseClass . '__title'],
         "autopromote" => true
     ])
         {!! $title !!}
     @endtypography
+    @endif
+    
+@if($meta)
+    @typography([
+        'element' => "h3",
+        'classList' => [$baseClass . '__meta'],
+    ])
+    {!! $meta !!}
+    @endtypography
+@endif
+@if($tags) 
+    @tags([
+        'tags' => $tags,
+        'classList' => [$baseClass . '__tags', 'u-margin__top--1'],
+    ])
+    @endtags
 @endif
 
 @if($content)
--- source/php/Component/Segment/partials/test.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="{{$baseClass}}__content o-container o-container--content o-container--keep-spacing">
-    <div class="{{$baseClass}}__padder">
-        @include('Segment.components.content')
-    </div>
-</div>
