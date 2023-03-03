d45e54940ab3b595d98f12545b6d25fd94195e84
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-03 21:21:58 +0100

    Always use sm size in submenus. Remove old is-expanded boolean.

--- source/php/Component/Nav/children.blade.php
+++ source/php/Component/Nav/children.blade.php
@@ -1,10 +1,10 @@
 @if(is_array($item['children']))
   @nav([
     'items' => $item['children'],
-    'isExpanded' => ((isset($item['active']) && boolval($item['active'])) || (isset($item['ancestor']) &&  boolval($item['ancestor']) )) ? true : false,
     'includeToggle' => $includeToggle,
     'depth' =>  $depth + 1,
     'direction' => 'vertical',
+    'height' => 'sm',
     'classList' => [
       $baseClass . '--bordered'
     ]
