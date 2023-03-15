6c3d9e52ef3b019d679b2c2c2ddcf2e685d18c88
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-15 13:46:55 +0100

    Introduce tiles style in nav menus

---
+++ source/php/Component/Nav/style/tiles.blade.php
@@ -0,0 +1,16 @@
+@if($item['icon'] || $item['label'])
+  @link([
+    'id' => $id . '-' . $item['id'] . '-' . $loop->index . '__label',
+    'classList' => [
+        $baseClass . '__link',
+        'c-paper',
+        'u-margin__bottom--1'
+    ],
+    'href' => $item['href'],
+  ])
+    @icon($item['icon'])@endicon
+    <span class="{{$baseClass}}__text">{{$item['label']}}</span>
+  @endlink
+@else
+  <!-- Hidden link: Both label and icon is missing -->
+@endif
