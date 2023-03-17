674df63b009af48352f55f1692fa43f26d6291db
NiclasNorin <niclas.norin@frontwalker.se>
2023-03-17 11:43:28 +0100

    allow innerblock segment new view

--- source/php/Component/Segment/partials/card.blade.php
+++ source/php/Component/Segment/partials/card.blade.php
@@ -3,5 +3,6 @@
         'classList' => [$baseClass . '__content-card']
     ])
     @include('Segment.components.content')
+    <div class="{{$baseClass}}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
     @endcard
 </div>
