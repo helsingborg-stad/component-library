1816db129b444fbe0de13a3057b0d486a5f4162a
Anna Johansson <anna@thehappyhipster.se>
2023-03-21 11:06:14 +0100

    Accept array preselected

--- source/php/Component/Select/Select.php
+++ source/php/Component/Select/Select.php
@@ -4,8 +4,8 @@ namespace ComponentLibrary\Component\Select;
 
 class Select extends \ComponentLibrary\Component\BaseController
 {
-
-    public function init() {
+    public function init()
+    {
 
         //Extract array for eazy access (fetch only)
         extract($this->data);
@@ -19,6 +19,15 @@ class Select extends \ComponentLibrary\Component\BaseController
 
         if ($multiple) {
             $this->data['attributeList']['multiple'] = 'multiple';
+        }
+
+        $this->data['intersection'] = [];
+        if (is_array($preselected) && !empty($options)) {
+            // Create an associative array from $preselected with keys same as values
+            $preselectedKeys = array_flip($preselected);
+
+            // Check if $options contains any items that have the key of a $preselected value
+            $this->data['intersection'] = array_intersect_key($options, $preselectedKeys);
         }
 
         if ($name) {
