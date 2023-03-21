3e8b7972c5b9b9acbc9801dcdcae9db9cece6e53
NiclasNorin <niclas.norin@frontwalker.se>
2023-03-21 08:23:32 +0100

    remove comment

--- source/php/Component/Card/partials/body.blade.php
+++ source/php/Component/Card/partials/body.blade.php
@@ -3,7 +3,6 @@
     @includeWhen($meta && $metaFirst, 'Card.components.meta')
     @includeWhen($heading || ($meta && !$metaFirst) || $icon, 'Card.components.heading')
     @includeWhen($subHeading, 'Card.components.subHeading')
-    {{-- @includeWhen($meta && !$metaFirst, 'Card.components.meta') --}}
     @includeWhen($date, 'Card.components.date')
     @includeWhen($content, 'Card.components.content')
  </div>
