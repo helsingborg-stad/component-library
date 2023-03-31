d35c4deb606d78b97d900218d9d7819c561e61db
Anna Johansson <anna@thehappyhipster.se>
2023-03-31 10:56:26 +0200

    Add term color to icons on tags

--- source/php/Component/Tags/tags.blade.php
+++ source/php/Component/Tags/tags.blade.php
@@ -1,38 +1,38 @@
-@if(!empty($tags))
-    <{{ $componentElement }} id="{{$uid}}" class="{{ $class }}" {!! $attribute !!}>
-        @foreach($tags as $tag)
-            @if(isset($tag['href']) && !empty($tag['href']))
+@if (!empty($tags))
+    <{{ $componentElement }} class="{{ $class }}" id="{{ $uid }}" {!! $attribute !!}>
+        @foreach ($tags as $tag)
+            @if (isset($tag['href']) && !empty($tag['href']))
                 @link([
-                    'href' => $tag['href'], 
-                    'classList' => [
-                        'c-tags__tag', 
-                        $isHidden($loop->index)
-                    ]
+                    'href' => $tag['href'],
+                    'classList' => ['c-tags__tag', $isHidden($loop->index)]
                 ])
-                    {{ $beforeLabel }}{{ $tag['label'] ?? 'Undefined label' }}{{ $afterLabel }}
+                    {{ $beforeLabel }}
+                    {{ $tag['label'] ?? 'Undefined label' }}
+                    {{ $afterLabel }}
                 @endlink
-            @else 
-
+            @else
                 <span class="c-tags__tag {{ $isHidden($loop->index) }}">
                     @if (!empty($icon))
                         @icon([
                             'icon' => $icon['icon'],
                             'size' => $icon['size'],
+                            'customColor' => $tag['color']
                         ])
                         @endicon
                     @else
                         {{ $beforeLabel }}
                     @endif
-                    {{ $tag['label'] ?? 'Undefined label' }}{{ $afterLabel }}
+                    {{ $tag['label'] ?? 'Undefined label' }}
+                    {{ $afterLabel }}
                 </span>
             @endif
         @endforeach
 
-        @if($compress && $compress < $tagCount)
+        @if ($compress && $compress < $tagCount)
             <span class="c-tags__tag c-tags__tag-more">
-                {{ "..." }}
+                {{ '...' }}
             </span>
         @endif
 
-    </{{ $componentElement }}>
+        </{{ $componentElement }}>
 @endif
