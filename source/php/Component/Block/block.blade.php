12f0f9ce6ee33fb4f25a5b077eca6d759ddc8730
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-05 09:17:50 +0200

    icon

--- source/php/Component/Block/block.blade.php
+++ source/php/Component/Block/block.blade.php
@@ -71,8 +71,13 @@
                     @endtypography
                 @endif
                 @if ($icon)
-                    @icon($icon)
-                    @endicon
+                    @inlineCssWrapper([
+                        'styles' => ['background-color' => $icon['backgroundColor'], 'display' => 'flex'],
+                        'classList' => [$icon['backgroundColor'] ? '' : 'u-color__bg--primary', 'u-rounded--full', 'u-detail-shadow-3']
+                    ])
+                        @icon($icon)
+                        @endicon
+                    @endinlineCssWrapper
                 @endif
             @endgroup
             @endif
--- source/php/Component/Segment/components/content.blade.php
+++ source/php/Component/Segment/components/content.blade.php
@@ -28,9 +28,14 @@
     @endif
     @endgroup
 
-    @if($icon)
-        @icon($icon)
-        @endicon
+       @if ($icon)
+        @inlineCssWrapper([
+            'styles' => ['background-color' => $icon['backgroundColor'], 'display' => 'flex'],
+            'classList' => [$icon['backgroundColor'] ? '' : 'u-color__bg--primary', 'u-rounded--full', 'u-detail-shadow-3']
+        ])
+            @icon($icon)
+            @endicon
+        @endinlineCssWrapper
     @endif
 @endgroup
 @endif
