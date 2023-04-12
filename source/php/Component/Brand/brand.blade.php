ee74e3dd4d4e7359ae3165ce4e1709b55b232dfe
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2023-04-12 15:52:19 +0200

    Add brand component (#245)
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ source/php/Component/Brand/brand.blade.php
@@ -0,0 +1,34 @@
+<!-- brand.blade.php -->
+@if($text)
+<div class="{{ $class }}" {!! $attribute !!}>
+    <svg viewBox="0 0 500 100" preserveAspectRatio="xMinYMin meet" class="{{ $baseClass }}__viewbox">
+        <foreignObject width="100%" height="100%" xmlns="http://www.w3.org/1999/xhtml">
+            <div class="{{ $baseClass }}__container" xmlns="http://www.w3.org/1999/xhtml">
+
+                @if($logotype)
+                    @logotype($logotype)
+                    @endlogotype
+                @endif
+
+                <div class="{{ $baseClass }}__text">
+                    @foreach($text as $line)
+                        @typography([
+                            "variant" => "brand", 
+                            "element" => "span",
+                            "classList" => [
+                                $baseClass . "__line", 
+                                $baseClass . "__line--" . $loop->iteration
+                            ]
+                        ])
+                            {{ $line }}
+                        @endtypography
+                    @endforeach
+                </div>
+            </div>
+        </foreignObject>
+    </svg>
+</div>
+@else 
+    @logotype($logotype)
+    @endlogotype
+@endif
