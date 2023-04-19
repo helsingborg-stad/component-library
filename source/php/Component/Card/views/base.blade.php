32050225f82a74a6ffaf90b40c2143f7cc46acd1
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-19 14:58:50 +0200

    floating below image

--- source/php/Component/Card/views/base.blade.php
+++ source/php/Component/Card/views/base.blade.php
@@ -1,9 +1,9 @@
+@includeWhen($image && !empty($image['src']), 'Card.components.image')
 @if($floatingSlotHasData)
 <div class="{{$baseClass}}__floating">
     {!! $floating !!}
 </div>
 @endif
-@includeWhen($image && !empty($image['src']), 'Card.components.image')
 @includeWhen($image && !empty($image['src']) && $dateBadge && $date, 'Card.components.datebadge')
 @includeWhen($collapsible || $heading || $subHeading || $meta || ($date && !$dateBadge) || $content, 'Card.partials.body')
 @includeWhen($tags || $buttons, 'Card.partials.footer')
