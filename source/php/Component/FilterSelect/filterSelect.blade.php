bf070f51bfe77ae9a01b6c1a4244e9b04d7c3487
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-20 16:12:41 +0200

    filterselect baseclass instead of writing class

--- source/php/Component/FilterSelect/filterSelect.blade.php
+++ source/php/Component/FilterSelect/filterSelect.blade.php
@@ -5,15 +5,15 @@
         'options' => $options,
         'name' => $name,
         'preselected' => $preselected,
-        'classList' => ['c-filterselect__select']
+        'classList' => [$baseClass . '__select']
     ])
     @endselect
-    <div class="c-filterselect__dropdown">
-        <div class="c-filterselect__expand-button">
-            <div class="c-filterselect__checked-items">
-                <span class="c-filterselect__placeholder">{{ $placeholder }}</span>
+    <div class="{{$baseClass}}__dropdown">
+        <div class="{{$baseClass}}__expand-button">
+            <div class="{{$baseClass}}__checked-items">
+                <span class="{{$baseClass}}__placeholder">{{ $placeholder }}</span>
                 <template>
-                    <div class="c-filterselect__checked-item">
+                    <div class="{{$baseClass}}__checked-item">
                         {OPTION_LABEL}
                         @icon([
                             'size' => 'sm',
@@ -26,30 +26,30 @@
             @icon([
                 'icon' => 'expand_less',
                 'size' => 'md',
-                'classList' => ['c-filterselect__expand-less-icon']
+                'classList' => [$baseClass . '__expand-less-icon']
             ])
             @endicon
             @icon([
                 'icon' => 'expand_more',
                 'size' => 'md',
-                'classList' => ['c-filterselect__expand-more-icon']
+                'classList' => [$baseClass . '__expand-more-icon']
             ])
             @endicon
         </div>
-        <div class="c-filterselect__options">
+        <div class="{{$baseClass}}__options">
             @foreach ($options as $value => $name)
-                <div class="c-filterselect__option" js-select-value="{{ $value }}">
-                    <span class="c-filterselect__option-label">{{ $name }}</span>
+                <div class="{{$baseClass}}__option" js-select-value="{{ $value }}">
+                    <span class="{{$baseClass}}__option-label">{{ $name }}</span>
                     @icon([
                         'icon' => 'check_box_outline_blank',
                         'size' => 'md',
-                        'classList' => ['c-filter__unchecked-icon']
+                        'classList' => [$baseClass . '__unchecked-icon']
                     ])
                     @endicon
                     @icon([
                         'icon' => 'check_box',
                         'size' => 'md',
-                        'classList' => ['c-filter__checked-icon']
+                        'classList' => [$baseClass . '__checked-icon']
                     ])
                     @endicon
                 </div>
