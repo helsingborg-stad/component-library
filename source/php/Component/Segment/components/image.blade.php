8c7e7014cd81b5ed39a4e35759d93b7a6ae50fbe
Anna Johansson <anna@thehappyhipster.se>
2023-04-24 10:18:34 +0200

    Link the image if there is only one button

--- source/php/Component/Segment/components/image.blade.php
+++ source/php/Component/Segment/components/image.blade.php
@@ -1,1 +1,10 @@
-<div class="{{$baseClass}}__image {{ $imageClass }}" style="{{ $imageStyleString }}"></div>
+@if ($buttons && count($buttons) === 1)
+    @link([
+        'href' => $buttons[0]['href'],
+        'classList' => ['c-segment__image']
+    ])
+    @endif
+    <div class="{{ $baseClass }}__image {{ $imageClass }}" style="{{ $imageStyleString }}"></div>
+    @if ($buttons && count($buttons) === 1)
+    @endlink
+@endif
