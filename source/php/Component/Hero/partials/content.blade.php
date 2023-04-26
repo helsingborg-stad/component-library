def19f753776dc0fab37fed8e125faf6b1800a31
Thor Brink <thor.brink@helsingborg.se>
2023-04-26 08:19:25 +0200

    Disable Hero content if no content (#271)
    * fix: no hero content if content strings are empty
    
    * test: fix broken hero test
    
    * test: handle broken path to hero.json
    
    * test: all cases for Hero hasContent

---
+++ source/php/Component/Hero/partials/content.blade.php
@@ -0,0 +1,41 @@
+<div class="{{ $baseClass }}__content" style="{{ $contentStyles }}">
+                
+    @if( $linkArgs )
+        @link($linkArgs)
+    @endif
+
+    @if ($meta)
+        @typography(['classList' => [$baseClass . '__meta']])
+            {!! $meta !!}
+        @endtypography
+    @endif
+    @if ($title)
+        @typography(['variant' => 'h1', 'element' => 'h1', 'classList' => [$baseClass . '__title']])
+            {!! $title !!}
+        @endtypography
+    @endif
+
+    @if ($byline)
+        @typography(['variant' => 'h2', 'element' => 'span', 'classList' => [$baseClass . '__byline']])
+            {!! $byline !!}
+        @endtypography
+    @endif
+
+    @if ($paragraph)
+        @typography(['variant' => 'p', 'element' => 'p', 'classList' => [$baseClass . '__body']])
+            {!! $paragraph !!}
+        @endtypography
+    @endif
+
+    @if( $linkArgs )
+        @endlink
+    @endif
+
+    @if( $buttonArgs )
+        @button($buttonArgs)@endbutton
+    @endif
+
+    {{-- Oneline to enable the use of css:empty() function --}}
+    <div class="{{ $baseClass }}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
+
+</div>
