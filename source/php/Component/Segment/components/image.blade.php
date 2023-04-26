8fc9f41967429565c72373639c3481238f79b07d
Anna Johansson <anna@thehappyhipster.se>
2023-04-26 10:32:51 +0200

    Use keepContent and keepWrapper parameters

--- source/php/Component/Segment/components/image.blade.php
+++ source/php/Component/Segment/components/image.blade.php
@@ -1,10 +1,8 @@
-@if ($link)
-    @link([
-        'href' => $link,
-        'classList' => ['c-segment__image']
-    ])
-    @endif
+@link([
+    'href' => $link,
+    'classList' => ['c-segment__image'],
+    'keepContent' => true,
+    'keepWrapper' => false
+])
     <div class="{{ $baseClass }}__image {{ $imageClass }}" style="{{ $imageStyleString }}"></div>
-    @if ($link)
-    @endlink
-@endif
+@endlink
