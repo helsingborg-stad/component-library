dc0099dba6fc83a2c61691d1e3ee5540ee8ffa6a
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-08 10:00:14 +0200

    openstreetmap tooltip content styling

--- source/php/Component/OpenStreetMap/components/tooltip.blade.php
+++ source/php/Component/OpenStreetMap/components/tooltip.blade.php
@@ -7,20 +7,20 @@
     {TOOLTIP_HEADING}
 @endtypography
 @typography([
-    'classList' => ['u-margin__top--1', 'c-openstreetmap__tooltip-excerpt'],
+    'classList' => ['u-margin__y--1', 'c-openstreetmap__tooltip-excerpt'],
     
 ])
     {TOOLTIP_EXCERPT}
 @endtypography
 @image([
-'classList' => ['c-openstreetmap__tooltip-image', 'u-margin__top--2'],
+'classList' => ['c-openstreetmap__tooltip-image', 'u-margin__top--1'],
 'src' => '{TOOLTIP_IMAGE_SRC}',
 'alt' => '{TOOLTIP_IMAGE_ALT}',
 ])
 @endimage
 @link([
 'href' => '{TOOLTIP_DIRECTIONS_URL}',
-'classList' => ['u-margin__top--2', 'c-openstreetmap__tooltip-directions']
+'classList' => ['c-openstreetmap__tooltip-directions']
 ])
 {TOOLTIP_DIRECTIONS_LABEL}
 @endlink
