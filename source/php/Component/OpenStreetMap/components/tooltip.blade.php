8fac483addfceb108bc47a03b659ed8bbdf8ef27
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-05-08 08:53:47 +0200

    Tooltip data init (#281)

--- source/php/Component/OpenStreetMap/components/tooltip.blade.php
+++ source/php/Component/OpenStreetMap/components/tooltip.blade.php
@@ -1,16 +1,27 @@
-@group([
-    'direction' => 'vertical',
-])
+<div class="c-openstreetmap__tooltip">
 @typography([
+    'classList' => ['c-openstreetmap__tooltip-title'],
     'element' => 'h2',
     'variant' => 'h5',
 ])
     {TOOLTIP_HEADING}
 @endtypography
-@link([
-    'href' => '{TOOLTIP_DIRECTIONS_URL}',
-    'classList' => ['u-margin__top--1']
+@typography([
+    'classList' => ['u-margin__top--1', 'c-openstreetmap__tooltip-excerpt'],
+    
 ])
-    {TOOLTIP_DIRECTIONS_LABEL}
+    {TOOLTIP_EXCERPT}
+@endtypography
+@image([
+'classList' => ['c-openstreetmap__tooltip-image', 'u-margin__top--2'],
+'src' => '{TOOLTIP_IMAGE_SRC}',
+'alt' => '{TOOLTIP_IMAGE_ALT}',
+])
+@endimage
+@link([
+'href' => '{TOOLTIP_DIRECTIONS_URL}',
+'classList' => ['u-margin__top--2', 'c-openstreetmap__tooltip-directions']
+])
+{TOOLTIP_DIRECTIONS_LABEL}
 @endlink
-@endgroup
+</div>
