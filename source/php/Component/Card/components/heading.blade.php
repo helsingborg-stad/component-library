3e20d9f51319b5dc50a7f741f466fa372e3a5110
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-24 12:15:28 +0200

    styling fixed

--- source/php/Component/Card/components/heading.blade.php
+++ source/php/Component/Card/components/heading.blade.php
@@ -3,24 +3,19 @@
     'alignItems' => 'start',
 ])
     @group([
-        'alignItems' => 'flex-end',
-    ])
-        @includeWhen(!$image || empty($image['src'] && ($dateBadge && $date)), 'Card.components.datebadge', ['datebadgeClasses' => ['u-margin__right--2']])
-    @group([
-        'display' => 'block'
+        'direction' => 'vertical'
     ])
         @typography([
             'element'   => 'h2',
             'variant'   => 'h3',
             'classList' => [
-                $baseClass."__heading",
-                'u-margin__top--0'
+                $baseClass."__heading"
             ]
         ])
             {!! $heading !!}
         @endtypography
+
         @includeWhen($meta && !$metaFirst, 'Card.components.meta')
-    @endgroup
     @endgroup
     @includeWhen($icon && !empty($displayIcon), 'Card.components.icon')
 @endgroup
--- source/php/Component/Card/views/base.blade.php
+++ source/php/Component/Card/views/base.blade.php
@@ -4,6 +4,6 @@
     {!! $floating !!}
 </div>
 @endif
-@includeWhen($image && !empty($image['src']) && $dateBadge && $date, 'Card.components.datebadge')
+@includeWhen($dateBadge && $date, 'Card.components.datebadge')
 @includeWhen($collapsible || $heading || $subHeading || $meta || $date || $content, 'Card.partials.body')
 @includeWhen($tags || $buttons, 'Card.partials.footer')
