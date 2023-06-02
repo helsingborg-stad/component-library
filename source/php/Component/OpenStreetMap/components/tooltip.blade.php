0e907cc28205944be82a2fdec1632fd1eb707baf
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2023-06-02 15:12:59 +0200

    Setting nofollow on all links and urls (#299)

--- source/php/Component/OpenStreetMap/components/tooltip.blade.php
+++ source/php/Component/OpenStreetMap/components/tooltip.blade.php
@@ -1,32 +1,41 @@
 <div class="c-openstreetmap__tooltip">
-@link([
-    'href' => '{TOOLTIP_LINK}',
-    'classList' => ['c-openstreetmap__tooltip-link']
-])
-@typography([
-    'classList' => ['c-openstreetmap__tooltip-title'],
-    'element' => 'h2',
-    'variant' => 'h5',
-])
-    {TOOLTIP_HEADING}
-@endtypography
-@endlink
-@typography([
-    'classList' => ['u-margin__y--1', 'c-openstreetmap__tooltip-excerpt'],
-    
-])
-    {TOOLTIP_EXCERPT}
-@endtypography
-@image([
-'classList' => ['c-openstreetmap__tooltip-image', 'u-margin__top--1'],
-'src' => '{TOOLTIP_IMAGE_SRC}',
-'alt' => '{TOOLTIP_IMAGE_ALT}',
-])
-@endimage
-@link([
-'href' => '{TOOLTIP_DIRECTIONS_URL}',
-'classList' => ['c-openstreetmap__tooltip-directions']
-])
-{TOOLTIP_DIRECTIONS_LABEL}
-@endlink
+    @link([
+        'href' => '{TOOLTIP_LINK}',
+        'classList' => ['c-openstreetmap__tooltip-link'],
+        'attributeList' => [
+            'rel' => 'nofollow'
+        ]
+    ])
+    @typography([
+        'classList' => ['c-openstreetmap__tooltip-title'],
+        'element' => 'h2',
+        'variant' => 'h5',
+    ])
+        {TOOLTIP_HEADING}
+    @endtypography
+    @endlink
+    @typography([
+        'classList' => ['u-margin__y--1', 'c-openstreetmap__tooltip-excerpt'],
+        
+    ])
+        {TOOLTIP_EXCERPT}
+    @endtypography
+    @image([
+        'classList' => ['c-openstreetmap__tooltip-image', 'u-margin__top--1'],
+        'src' => '{TOOLTIP_IMAGE_SRC}',
+        'alt' => '{TOOLTIP_IMAGE_ALT}',
+        'attributeList' => [
+            'rel' => 'nofollow'
+        ]
+    ])
+    @endimage
+    @link([
+        'href' => '{TOOLTIP_DIRECTIONS_URL}',
+        'classList' => ['c-openstreetmap__tooltip-directions'],
+        'attributeList' => [
+            'rel' => 'nofollow'
+        ]
+    ])
+        {TOOLTIP_DIRECTIONS_LABEL}
+    @endlink
 </div>
