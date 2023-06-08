ca1d772199a683876247f6cfd957e2eb3545d0dc
NiclasNorin <niclas.norin@frontwalker.se>
2023-06-08 14:20:03 +0200

    data-js attributes

--- source/php/Component/OpenStreetMap/OpenStreetMap.php
+++ source/php/Component/OpenStreetMap/OpenStreetMap.php
@@ -22,7 +22,7 @@ class OpenStreetMap extends \ComponentLibrary\Component\BaseController
             $pin['lat'] = strval($pin['lat']);
             $pin['lng'] = strval($pin['lng']);
         }
-            $this->data['attributeList']['js-map-pin-data'] = json_encode($pins, JSON_UNESCAPED_UNICODE);
+            $this->data['attributeList']['data-js-map-pin-data'] = json_encode($pins, JSON_UNESCAPED_UNICODE);
 
         }
 
@@ -44,10 +44,10 @@ class OpenStreetMap extends \ComponentLibrary\Component\BaseController
                 'zoom' => '14',
             ];
         }
-        $this->data['attributeList']['js-map-start-position'] = json_encode($startPosition);
+        $this->data['attributeList']['data-js-map-start-position'] = json_encode($startPosition);
 
         if (!empty($mapStyle)) {
-            $this->data['attributeList']['js-map-style'] = $mapStyle;
+            $this->data['attributeList']['data-js-map-style'] = $mapStyle;
         }
 
         if (!empty($title)) {
--- source/php/Component/OpenStreetMap/partials/sidebar.blade.php
+++ source/php/Component/OpenStreetMap/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="{{$baseClass}}__sidebar" data-observe-resizes>
-    <div class="{{$baseClass}}__container" js-pagination-target>
+    <div class="{{$baseClass}}__container" data-js-pagination-target>
         <div class="{{$baseClass}}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
         {!! $sidebarContent !!}
     </div>
