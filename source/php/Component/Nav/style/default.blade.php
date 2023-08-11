4f582976ade49188c62b9e0f13ea3a292345f654
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-11 08:47:16 +0200

    Resolve some undefined issues in navigation color's

--- source/php/Component/Nav/style/default.blade.php
+++ source/php/Component/Nav/style/default.blade.php
@@ -5,13 +5,19 @@
     'href' => $item['href'],
   ])
     @icon([
-      'icon' => $item['icon']['icon'],
-      'size' => $item['icon']['size'],
-      'classList' => $item['icon']['classList'],
-      'attributeList' => ['style' => $item['color'] ? 'background-color:' . $item['color'] . ';' : '']
+      'icon' => $item['icon']['icon'] ?? null,
+      'size' => $item['icon']['size'] ?? null,
+      'classList' => $item['icon']['classList'] ?? [],
+      'attributeList' => [
+        'style' => 'background-color:' . ($item['color'] ?? '') . ';'
+      ]
     ])
     @endicon
-    <span class="{{$baseClass}}__text" style="{{$item['color'] ? 'color:' . $item['color'] . ';' : ''}}">{{$item['label']}}</span>
+    <span 
+      class="{{$baseClass}}__text" 
+      style="{{isset($item['color']) ? 'color:' . $item['color'] . ';' : ''}}">
+      {{$item['label']}}
+    </span>
   @endlink
 @else
   <!-- Hidden link: Both label and icon is missing -->
