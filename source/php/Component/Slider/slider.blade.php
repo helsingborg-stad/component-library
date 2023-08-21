3ef82189afa72ce074e8ae6d6832145fbad3e534
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-21 13:22:04 +0200

    Revert "Check that arrowButtons is an array before use."
    This reverts commit c503e2c98f614b21a7c9f71811026fb86b0583c6.

--- source/php/Component/Slider/slider.blade.php
+++ source/php/Component/Slider/slider.blade.php
@@ -1,25 +1,22 @@
 <div class="{{ $class }}" {!! $attribute !!}>
 
     @if(!$isPost || !$customButtons)
-
-        @if(is_array($arrowButtons))
-            <div class="splide__arrows c-slider__arrows">
-                @button([
-                    'color' => $arrowButtons['color'],
-                    'icon' => 'keyboard_arrow_left',
-                    'style' => $arrowButtons['style'],
-                    'classList' => ['splide__arrow', 'splide__arrow--prev']
-                ])
-                @endbutton
-                @button([
-                    'color' => $arrowButtons['color'],
-                    'icon' => 'keyboard_arrow_right',
-                    'style' => $arrowButtons['style'],
-                    'classList' => ['splide__arrow', 'splide__arrow--next']
-                ])
-                @endbutton
-            </div>
-        @endif
+        <div class="splide__arrows c-slider__arrows">
+            @button([
+                'color' => $arrowButtons['color'],
+                'icon' => 'keyboard_arrow_left',
+                'style' => $arrowButtons['style'],
+                'classList' => ['splide__arrow', 'splide__arrow--prev']
+            ])
+            @endbutton
+            @button([
+                'color' => $arrowButtons['color'],
+                'icon' => 'keyboard_arrow_right',
+                'style' => $arrowButtons['style'],
+                'classList' => ['splide__arrow', 'splide__arrow--next']
+            ])
+            @endbutton
+        </div>
     
         <div class="{{ $baseClass }}__container splide__track">
             <div class="{{ $baseClass }}__inner splide__list" js-slider-inner>
