2d113a9d3324c9fa3486a4086eae6490d2d70b79
Niclas Norin <niclas.norin@helsingborg.se>
2023-08-23 11:22:23 +0200

    Fix: Adding !empty instead of variable as condition

--- source/php/Component/Product/product.blade.php
+++ source/php/Component/Product/product.blade.php
@@ -1,11 +1,11 @@
 <!-- product.blade.php -->
 <div class="{{ $class }}" {!! $attribute !!}>
     @card()
-        @includeWhen($image && $image['src'], 'Product.components.image')
+        @includeWhen(!empty($image) && !empty($image['src']), 'Product.components.image')
         @includeWhen(!empty($heading) || !empty($label), 'Product.components.header')
         @includeWhen(!empty($meta) || !empty($bulletPoints), 'Product.components.body')
         <div class="c-card__footer {{$baseClass}}__footer">
-                @includeWhen($prices, 'Product.components.price')
+                @includeWhen(!empty($prices), 'Product.components.price')
                 @includeWhen(!empty($button), 'Product.components.button')
         </div>
     @endcard
