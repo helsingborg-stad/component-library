cb800abd347d738389d3bf358138f79ea57276b0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-25 11:18:04 +0200

    Mitigate warnings.

--- source/php/Component/Table/table.blade.php
+++ source/php/Component/Table/table.blade.php
@@ -43,71 +43,77 @@
         <div class="{{$baseClass}}__inner">
             <table class="{{$baseClass}}__table">
                 @if($showCaption)
-                    <caption>{{ $caption }}</caption>
+                    <caption>{{ $caption }}</caption>
                 @endif
 
                 @if($showHeader)
                     <thead class="{{$baseClass}}__head">                                                
                         <tr class="{{$baseClass}}__line">
-                            @foreach($headings as $heading)
-                                <th scope="col" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort--btn="{{ $loop->index }}">
-                                    
-                                    <span class="{{$baseClass}}__column-content">
+                            @if(!empty($headings))
+                                @foreach($headings as $heading)
+                                    <th scope="col" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort--btn="{{ $loop->index }}">
+                                        
+                                        <span class="{{$baseClass}}__column-content">
 
-                                        <!-- Heading label -->
-                                        <span class="{{$baseClass}}__heading">
-                                            {{ $heading }}
-                                        </span>
+                                            <!-- Heading label -->
+                                            <span class="{{$baseClass}}__heading">
+                                                {{ $heading }}
+                                            </span>
 
-                                        <!-- Collapse button -->
-                                        @if($isMultidimensional && $loop->index === 0)
-                                            @icon([
-                                                'icon' => 'chevron_left',
-                                                'size' => 'md',
-                                                'classList' => [$baseClass . '__collapse-button']
-                                            ])
-                                            @endicon
-                                        @endif
-
-                                        <!-- Sort button -->
-                                        @if($sortable)
-                                            @if(($isMultidimensional && $loop->index !== 0) || !$isMultidimensional )                                        
-                                                @icon(['icon' => 'swap_vert', 'size' => 'md', 'classList' => [$baseClass . '__sort-button']])
+                                            <!-- Collapse button -->
+                                            @if($isMultidimensional && $loop->index === 0)
+                                                @icon([
+                                                    'icon' => 'chevron_left',
+                                                    'size' => 'md',
+                                                    'classList' => [$baseClass . '__collapse-button']
+                                                ])
                                                 @endicon
                                             @endif
-                                        @endif
 
-                                    </span>
+                                            <!-- Sort button -->
+                                            @if($sortable)
+                                                @if(($isMultidimensional && $loop->index !== 0) || !$isMultidimensional )                                        
+                                                    @icon(['icon' => 'swap_vert', 'size' => 'md', 'classList' => [$baseClass . '__sort-button']])
+                                                    @endicon
+                                                @endif
+                                            @endif
 
-                                </th>
-                            @endforeach
+                                        </span>
+
+                                    </th>
+                                @endforeach
+                            @endif
                         </tr>
                     </thead>
                 @endif
 
                 <tbody class="{{$baseClass}}__body" js-sort-data-container js-table-data-container>
-                    @foreach($list as $row) 
-                        <tr class="{{$baseClass}}__line {{$baseClass}}__line-{{ $loop->index }}" js-table-sort--sortable js-table-filter-item>
-                            @foreach($row['columns'] as $column) 
-                                @if($loop->first)
-                                    <th scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort-data="{{ $loop->index }}" js-table-filter-data>
-                                        @link([
-                                            'href' => (isset($row['href']) && !empty($row['href']) ? $row['href'] : false),
-                                            'classList' => [$baseClass . '__column-content'],
-                                        ])
-                                            {!! $column !!}
-                                        @endlink  
-                                    </th>
-                                @else
-                                    <td scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort-data="{{ $loop->index }}" js-table-filter-data>
-                                        <span class="{{$baseClass}}__column-content">
-                                            {!! $column !!}
-                                        </span>
-                                    </td>
+                    @if(!empty($list)) 
+                        @foreach($list as $row) 
+                            <tr class="{{$baseClass}}__line {{$baseClass}}__line-{{ $loop->index }}" js-table-sort--sortable js-table-filter-item>
+                                @if(!empty($row['columns'])) 
+                                    @foreach($row['columns'] as $column) 
+                                        @if($loop->first)
+                                            <th scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort-data="{{ $loop->index }}" js-table-filter-data>
+                                                @link([
+                                                    'href' => (isset($row['href']) && !empty($row['href']) ? $row['href'] : false),
+                                                    'classList' => [$baseClass . '__column-content'],
+                                                ])
+                                                    {!! $column !!}
+                                                @endlink  
+                                            </th>
+                                        @else
+                                            <td scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}" js-table-sort-data="{{ $loop->index }}" js-table-filter-data>
+                                                <span class="{{$baseClass}}__column-content">
+                                                    {!! $column !!}
+                                                </span>
+                                            </td>
+                                        @endif
+                                    @endforeach
                                 @endif
-                            @endforeach
-                        </tr>
-                    @endforeach
+                            </tr>
+                        @endforeach
+                    @endif
                 </tbody>                
             </table>
         </div>
