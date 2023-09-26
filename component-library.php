de6ec07befadfbfb1141c242697eee19abb40971
GitHub Action <>
2023-09-26 12:47:34 +0000

    build: update package file versions to 2.0.0

--- component-library.php
+++ component-library.php
@@ -4,7 +4,7 @@
  * Plugin Name:       component-library
  * Plugin URI:        (#plugin_url#)
  * Description:       A library of blade components
- * Version:           1.0.0
+ * Version: 2.0.0
  * Author:            Eric Rosenborg
  * Author URI:        (#plugin_author_url#)
  * License:           MIT
