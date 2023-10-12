6e68b7e329577b622c2f281dacd4baa5ee80809d
NiclasNorin <niclasn@outlook.com>
2023-10-12 09:40:49 +0200

    Feat: Removes possibility for multiple h1 on same page

--- source/php/Component/Typography/Typography.php
+++ source/php/Component/Typography/Typography.php
@@ -6,6 +6,7 @@ class Typography extends \ComponentLibrary\Component\BaseController
 {
 
     private static $numberOfItems = 0;
+    private static $headingExists = false;
 
     public function init() {
         //Extract array for easy access (fetch only)
@@ -15,7 +16,15 @@ class Typography extends \ComponentLibrary\Component\BaseController
         $this->data['isPromotedHeading'] = false;
 
         //If this is the first heading of the page, promote it to h1
-        if ($autopromote === true) {
+        if ($element = 'h1') {
+            if (!empty(self::$headingExists)) {
+                $this->data['element'] = 'h2';
+            } else {
+                self::$headingExists = true;
+            }
+        }
+
+        if ($autopromote === true && !empty(self::$autopromote)) {
             if (in_array($element, ['h1', 'h2', 'h3']) && self::$numberOfItems == 0) {
                 $this->data['isPromotedHeading'] = true;
                 $this->data['element'] = 'h1';
