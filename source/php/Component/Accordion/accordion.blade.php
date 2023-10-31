578dadfc0bcff1c369c70976db3be306a916f7c5
Niclas Norin <niclas.norin@helsingborg.se>
2023-10-31 15:26:02 +0100

    fix: same for accordion component

--- source/php/Component/Accordion/accordion.blade.php
+++ source/php/Component/Accordion/accordion.blade.php
@@ -2,9 +2,9 @@
 @if($list)
     <{{$componentElement}} class="{{ $class }}" js-expand-container {!! $attribute !!}>
     @foreach($list as $section)
-        <{{$sectionElement}} class="{{$baseClass}}__section" aria-label="{{$section['heading']}}">
+        <{{$sectionElement}} class="{{$baseClass}}__section">
 
-        <{{$sectionHeadingElement}} class="{{$baseClass}}__button" role="button" aria-controls="{{ $baseClass }}__aria-{{ $id }}-{{ $loop->index }}" aria-expanded="false" js-expand-button>
+        <{{$sectionHeadingElement}} class="{{$baseClass}}__button" role="button" aria-label="{{$section['heading']}}" aria-controls="{{ $baseClass }}__aria-{{ $id }}-{{ $loop->index }}" aria-expanded="false" js-expand-button>
         <span class="{{$baseClass}}__button-wrapper" tabindex="-1">
                 {!!$beforeHeading!!}
 
