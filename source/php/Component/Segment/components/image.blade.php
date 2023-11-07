00f36aee2a189b25dfb3e36919fe5e44fc51faab
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-11-07 14:16:36 +0100

    fix: resolve issue where object was used as an array

--- source/php/Component/Segment/components/image.blade.php
+++ source/php/Component/Segment/components/image.blade.php
@@ -4,7 +4,7 @@
     'keepContent' => true,
     'keepWrapper' => false,
     'attributeList' => [
-        'aria-label' => $lang['visit'] . ': ' . $link,
+        'aria-label' => $lang->visit . ': ' . $link,
     ]
 ])
     <div class="{{ $baseClass }}__image {{ $imageClass }}" style="{{ $imageStyleString }}"></div>
