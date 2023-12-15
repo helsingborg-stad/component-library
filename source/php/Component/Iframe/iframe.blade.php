f81108f65fdc71d8f221b2f6e283890227784a77
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-12-15 12:15:08 +0100

    Implement post message method for iframe resize.

--- source/php/Component/Iframe/iframe.blade.php
+++ source/php/Component/Iframe/iframe.blade.php
@@ -10,21 +10,22 @@
     ])
     <iframe id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
     </iframe>
-    @if($isSameDomain)
-        <script>
-            const acceptance    = document.querySelector('#acceptance-{{$id}}');
-            const iframe        = acceptance?.querySelector('#{{$id}}');
-            if(iframe) {
-                iframe.onload = function() {
-                    const iframeBody = iframe.contentWindow.document.body
-                    const iframeResizeObserver = new ResizeObserver(function() {
-                        const targetHeight = iframeBody.scrollHeight + 'px' || 'auto';
-                        acceptance.style.height = targetHeight;
-                        iframe.style.height = targetHeight;
-                    });
-                    iframeResizeObserver.observe(iframeBody);
+    <script>
+        window.addEventListener(
+            'message',
+            function(e) {
+                if (!event.origin.match('{{$embeddedDomain}}')) {
+                    return;
                 }
-            }
-        </script>
-    @endif
+                const acceptance    = document.querySelector('#acceptance-{{$id}}');
+                const iframe        = acceptance?.querySelector('#{{$id}}');
+                let message         = e.data;
+
+                if(iframe && message.height) {
+                    iframe.height = message.height;
+                }
+            },
+            false
+        );
+    </script>
 @endacceptance
