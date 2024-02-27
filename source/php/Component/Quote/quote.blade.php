f0dccb421f6194dab880cf0a31958afc6113f885
NiclasNorin <niclasn@outlook.com>
2024-02-27 12:28:02 +0100

    feat: quote layout

--- source/php/Component/Quote/quote.blade.php
+++ source/php/Component/Quote/quote.blade.php
@@ -1,18 +1,43 @@
 <div class="{{ $class }}" {!! $attribute !!}>
-    @if($content)
-        @typography([
+   @if($image)
+        @image([
+            'src' => $image->src,
+            'alt' => $image->alt,
+            'classList' => [
+                $baseClass . '__image'
+            ]
         ])
-            {{$content}}
-        @endtypography
-    @endif
-    @if($footer)
-        @typography([
-        ])
-            {{$footer}}
-        @endtypography
-    @endif
-    @if($image)
-        @image((array) $image)
         @endimage
+    @endif
+    @if ($content || $footer)
+        <div class="{{ $baseClass }}__content">
+            @icon([
+                'icon' => 'format_quote',
+                'classList' => [
+                    $baseClass . '__icon'
+                ]
+            ])
+            @endicon
+            @if($content)
+                <div class="{{$baseClass}}__quote">
+                    {!! $content !!}
+                </div>                
+            @endif
+
+            <div class="{{$baseClass}}__footer">
+                <div class="{{$baseClass}}__footer-content">
+                    @if($footer)
+                        {!! $footer !!}
+                    @endif
+                </div>
+                @icon([
+                    'icon' => 'format_quote',
+                    'classList' => [
+                        $baseClass . '__icon'
+                    ]
+                ])
+                @endicon
+            </div>
+        </div>
     @endif
 </div>
