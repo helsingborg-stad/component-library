588be32699dd979f2a5a3423ae7f5bf5ba7919fd
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-03-15 10:25:51 +0100

    fix: define compParams in icon component

--- source/php/Component/Icon/Icon.php
+++ source/php/Component/Icon/Icon.php
@@ -13,6 +13,7 @@ class Icon extends \ComponentLibrary\Component\BaseController
         'key'  => "Label"
     ];
     private $altTextUndefined = "Undefined";
+    private $compParams = [];
 
     public function init()
     {
