b15b4372e2d15ab85fa7e9f52aba45eb774b581b
NiclasNorin <niclasn@outlook.com>
2024-05-02 10:28:17 +0200

    fix: only setting default icon aria label if none exists

--- source/php/Component/Icon/Icon.php
+++ source/php/Component/Icon/Icon.php
@@ -56,12 +56,13 @@ class Icon extends \ComponentLibrary\Component\BaseController
 
         //Identify as an image
         $this->data['attributeList']['role'] = "img";
-        $this->data['attributeList']['aria-label'] = $this->getAltText($icon);
-        $this->data['attributeList']['alt'] = $this->getAltText($icon);
         $this->data['attributeList']['data-nosnippet'] = "";
         $this->data['attributeList']['translate'] = "no";
         $this->data['attributeList']['aria-hidden'] = "true";
 
+        if (empty($this->data['attributeList']['aria-label'])) {
+            $this->data['attributeList']['aria-label'] = $this->getAltText($icon);
+        }
     }
 
     private function iconIsSvg($icon)
