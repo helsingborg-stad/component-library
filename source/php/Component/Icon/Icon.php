275399f08bb9143b5c79f493a4977a802f97cb27
NiclasNorin <niclasn@outlook.com>
2024-05-03 11:17:05 +0200

    fix: removing alt text and aria label

--- source/php/Component/Icon/Icon.php
+++ source/php/Component/Icon/Icon.php
@@ -56,11 +56,15 @@ class Icon extends \ComponentLibrary\Component\BaseController
 
         //Identify as an image
         $this->data['attributeList']['role'] = "img";
-        $this->data['attributeList']['aria-label'] = $this->getAltText($icon);
-        $this->data['attributeList']['alt'] = $this->getAltText($icon);
         $this->data['attributeList']['data-nosnippet'] = "";
         $this->data['attributeList']['translate'] = "no";
-        $this->data['attributeList']['aria-hidden'] = $decorative ? "true" : "false";
+
+        if ($decorative) {
+            $this->data['attributeList']['aria-hidden'] = "true";
+        } else {
+            $this->data['attributeList']['aria-label'] = $this->getAltText($icon);
+            $this->data['attributeList']['alt'] = $this->getAltText($icon);
+        }
 
     }
 
