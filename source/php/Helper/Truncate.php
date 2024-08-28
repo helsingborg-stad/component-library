070f231fef7d41dd2583869a73958a54a5de8263
Niclas <niclasn@outlook.com>
2024-08-28 13:35:42 +0200

    feat: truncate and breadcrumb using tooltip

---
+++ source/php/Helper/Truncate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ComponentLibrary\Helper;
+
+class Truncate {
+    public static function truncate(string $content, int $length, string $suffix = '...'): string
+    {
+        if (strlen($content) <= $length) {
+            return $content;
+        }
+
+        return substr($content, 0, $length) . $suffix;
+    }
+}
