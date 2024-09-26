90e95302d8cbde51471578e6ac584fd92c77fc86
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-26 09:41:59 +0200

    fix: remove wrapper.

--- source/php/Component/Card/components/image.blade.php
+++ source/php/Component/Card/components/image.blade.php
@@ -1,9 +1,6 @@
-<div class="{{$baseClass}}__image">
-    @image([
-        'src' => (is_array($image) && isset($image['src'])) ? $image['src'] : $image,
-        'alt' => $alt ?? null,
-        'classList' => [$baseClass . '__image'],
-        'cover' => true
-    ])
-    @endimage
-</div>
+@image([
+    'src' => (is_array($image) && isset($image['src'])) ? $image['src'] : $image,
+    'alt' => $alt ?? null,
+    'classList' => [$baseClass . '__image']
+])
+@endimage
