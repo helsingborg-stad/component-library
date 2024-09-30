cfe4ae1dfa8e7156f1e0290936c4781615fadf23
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-30 12:14:01 +0200

    fix: add class to image component.

--- source/php/Component/Slider__item/sub/image.blade.php
+++ source/php/Component/Slider__item/sub/image.blade.php
@@ -1,7 +1,8 @@
 @image([
-    'src' => $image,
-    'alt' => $alt ?? null,
-    'cover' => true,
-    'context' => ['component.slider', 'component.slider.image']
+  'classList' => [$baseClass.'__image']
+  'src' => $image,
+  'alt' => $alt ?? null,
+  'cover' => true,
+  'context' => ['component.slider', 'component.slider.image']
 ])
 @endimage
