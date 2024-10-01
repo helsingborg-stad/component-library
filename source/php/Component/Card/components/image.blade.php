f0885faed1a291aa1745a4dc26df43b18ff82089
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-01 08:06:15 +0200

    fix: replace background image with image

--- source/php/Component/Card/components/image.blade.php
+++ source/php/Component/Card/components/image.blade.php
@@ -1,3 +1,14 @@
-<div class="{{$baseClass}}__image {{$baseClass}}__image--{{$image['backgroundColor']}}">
+<!-- <div class="{{$baseClass}}__image {{$baseClass}}__image--{{$image['backgroundColor']}}">
     <div class="{{$baseClass}}__image-background {{$paddedImage}}" style="background-image:url('{{$image['src']}}');"></div>
+</div> -->
+<div class="{{$baseClass}}__image-container">
+    @image([
+        'src' => $image['src'],
+        'alt' => $image['alt'],
+        'cover' => true,
+        'classList' => [
+            $baseClass . '__image'
+        ]
+    ])
+    @endimage
 </div>
