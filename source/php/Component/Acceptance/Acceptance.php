e2ea11b8ca582dac3091bcaddb088c358af5defb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-03 14:38:14 +0200

    fix: do not allow empty background filenames to appear.

--- source/php/Component/Acceptance/Acceptance.php
+++ source/php/Component/Acceptance/Acceptance.php
@@ -41,8 +41,7 @@ class Acceptance extends \ComponentLibrary\Component\BaseController
             }
         }
         
-        if ($cover)
-        {
+        if ($cover && $this->isValidBackgroundAssetName($cover)) {
             $this->data['attributeList']['style'][] = "background-image:url(\"$cover\");background-size:cover";
         }
 
@@ -246,6 +245,22 @@ class Acceptance extends \ComponentLibrary\Component\BaseController
         }
 
         return $this->data;
+    }
+
+    /**
+     * Check if image is a valid background asset name
+     * 
+     * @param string $name
+     * 
+     * @return bool
+     */
+    private function isValidBackgroundAssetName($name): bool
+    {
+        $name = pathinfo($name, PATHINFO_FILENAME);
+        if(empty($name)) {
+            return false;
+        }
+        return true; 
     }
 }
 
