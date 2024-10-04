fd3e2f5e964f492aea1f38b1cf971e8d4b64837a
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-10-04 16:54:42 +0200

    feat: implement container query with portrait support. (#460)
    * feat: implement container query with portrait support.
    
    * fix: remove container type inline css. Implemented in styleguide.
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ source/php/Component/Image/sub/mode/container.blade.php
@@ -0,0 +1,22 @@
+<!-- Image assets -->
+@foreach($containerQueryData as $item)
+  <img 
+    loading="lazy" 
+    class="{{$baseClass}}__image {{$baseClass}}--{{$item['uuid']}}" 
+    src="{{$item['url']}}"
+    alt="{{$alt}}"
+    style="{{$focus}}"
+    {!! $imgAttributes !!}
+  />
+@endforeach
+
+<!-- Image styles -->
+<style>
+  @foreach($containerQueryData as $item)
+    @foreach(['landscape', 'portrait'] as $dimension)
+      @container {{$item['media'][$dimension]}} and (orientation: {{$dimension}}) {
+          .{{$baseClass}}.{{$baseClass}}--container-query .{{$baseClass}}--{{$item['uuid']}} {display: block;}
+      }
+    @endforeach
+  @endforeach
+</style>
