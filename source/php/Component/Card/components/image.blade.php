f14d15891a3b0430edb42cc15ebfc7826b8efd3b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-10 11:44:34 +0200

    fix: autofill placeholders in cards.

--- source/php/Component/Card/components/image.blade.php
+++ source/php/Component/Card/components/image.blade.php
@@ -1,14 +1,13 @@
-@if($imageExists)
-    <div class="{{$baseClass}}__image-container">
-        @image([
-            'src' => is_array($image) ? ($image['src'] ?? null) : $image,
-            'alt' => is_array($image) ? ($image['alt'] ?? null) : null,
-            'cover' => true,
-            'classList' => [
-                $baseClass . '__image'
-            ],
-            'placeholderEnabled' => $hasPlaceholder,
-        ])
-        @endimage
-    </div>
-@endif
+
+<div class="{{$baseClass}}__image-container">
+    @image([
+        'src' => is_array($image) ? ($image['src'] ?? null) : $image,
+        'alt' => is_array($image) ? ($image['alt'] ?? null) : null,
+        'cover' => true,
+        'classList' => [
+            $baseClass . '__image'
+        ],
+        'placeholderEnabled' => $hasPlaceholder,
+    ])
+    @endimage
+</div>
