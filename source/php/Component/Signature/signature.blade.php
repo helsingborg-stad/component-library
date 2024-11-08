d91f47b8e96f3f0e00d55e4bdd366778f26ab912
Niclas <niclasn@outlook.com>
2024-11-08 11:36:36 +0100

    feat: signature slot

--- source/php/Component/Signature/signature.blade.php
+++ source/php/Component/Signature/signature.blade.php
@@ -34,7 +34,9 @@
                     ])
                         {{$authorRole}}
                     @endtypography
+
                 @endif
+                    {!! $slot !!}
             </div>
         </div>
     @endif
