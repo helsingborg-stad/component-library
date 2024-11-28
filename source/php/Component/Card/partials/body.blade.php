a4a6edf049b1cd1e51929917fd24d4ebe1d71274
Thor Brink <thor.brink@helsingborg.se>
2024-11-28 15:00:52 +0100

    fix: image as array warning (#484)

--- source/php/Component/Card/partials/body.blade.php
+++ source/php/Component/Card/partials/body.blade.php
@@ -6,7 +6,7 @@
         </div>
     @endif
     @includeWhen($meta && $metaFirst, 'Card.components.meta')
-    @includeWhen($heading || $subHeading || ($meta && !$metaFirst) || $icon || (!$image || empty($image['src'] && ($dateBadge && $date))), 'Card.components.heading')
+    @includeWhen($heading || $subHeading || ($meta && !$metaFirst) || $icon, 'Card.components.heading')
     @includeWhen($date && !$dateBadge, 'Card.components.date')
     @includeWhen($content, 'Card.components.content')
  </div>
