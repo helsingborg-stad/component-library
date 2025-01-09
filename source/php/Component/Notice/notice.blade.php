eb0299bffa0b1f32ef11bf193fe4d6531901b573
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2025-01-09 13:35:14 +0100

    feat: dismissable notices (#501)
    * feat: add markup for dismissable notices.
    
    * feat: add interface to component.
    
    * feat: implement data handling
    
    * fix: typing issues
    
    * fix: set label as key text
    
    * feat: add dismiss uid. fix: rendering of buttons in div.
    
    * fix: remove color
    
    ---------
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- source/php/Component/Notice/notice.blade.php
+++ source/php/Component/Notice/notice.blade.php
@@ -1,7 +1,7 @@
 <!-- notice.blade.php -->
 <div class="{{ $class }}" {!! $attribute !!} aria-labelledby="notice__text__{{ $id }}">
     
-    <!-- notice__ico -->
+    {{-- notice__ico --}}
     @if($icon)
         <span class="{{$baseClass}}__icon">
             @icon(['icon' => $icon['name'], 'size' => 'md'])
@@ -9,7 +9,7 @@
         </span>
     @endif
     
-    <!-- notice__title -->
+    {{-- notice__title --}}
     @if($message['title'])
         @typography([
             "variant" => "h4",
@@ -22,7 +22,7 @@
         @endtypography
     @endif
 
-    <!-- notice__text -->
+    {{-- notice__text --}}
     <span id="notice__text__{{ $id }}" for="" class="{{$baseClass}}__message">
         @if(isset($message['text']))
             {!! $message['text'] !!}
@@ -30,4 +30,44 @@
         {!! $slot !!}
     </span>
 
+    {{-- notice__actions --}}
+    @if($action || $dismissable)
+        <div class="{{$baseClass}}__actions">
+
+            {{-- notice__action --}}
+            @if($action)
+                <div class="{{$baseClass}}__action">
+                    @button([
+                        'text' => $action['text'] ?? 'Undefined',
+                        'href' => $action['url'] ?? '#',
+                        'style' => 'basic',
+                        'size' => 'sm',
+                        'classList' => [
+                            $baseClass . '__button'
+                        ],
+                    ])
+                    @endbutton
+                </div>
+            @endif
+
+            {{-- notice__dismiss --}}
+            @if($dismissable)
+                <div class="{{$baseClass}}__dismiss">
+                    @button([
+                        'icon' => 'close',
+                        'style' => 'basic',
+                        'size' => 'sm',
+                        'classList' => [
+                            $baseClass . '__dismiss'
+                        ],
+                        'attributeList' => [
+                            'data-dismiss' => 'notice',
+                            'aria-label' => 'Close'
+                        ]
+                    ])
+                    @endbutton
+                </div>
+            @endif
+        </div>
+    @endif
 </div>
