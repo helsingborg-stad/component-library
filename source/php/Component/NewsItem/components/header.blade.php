852a7d2b055139f80a4317b1c7c4ede1e5225979
Niclas <niclasn@outlook.com>
2025-01-21 09:56:15 +0100

    refactor: always render news item header left

--- source/php/Component/NewsItem/components/header.blade.php
+++ source/php/Component/NewsItem/components/header.blade.php
@@ -1,12 +1,12 @@
 <div class="{{$baseClass}}__header">
-    @if ($subHeading || $headerLeftAreaSlotHasData)
-        <div class="{{$baseClass}}__header-left">
+    <div class="{{$baseClass}}__header-left">
+        @if ($subHeading || $headerLeftAreaSlotHasData)
             @includeWhen($subHeading, 'NewsItem.partials.subHeading')
             @if ($headerLeftAreaSlotHasData)
                 {!! $headerLeftArea !!}
             @endif
-        </div>   
-    @endif
+        @endif
+    </div>
     @if ($headerRightAreaSlotHasData || $date || $readTime)
         <div class="{{$baseClass}}__header-right">
             @includeWhen($date, 'NewsItem.partials.date')
