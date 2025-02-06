eb827e95537dab97552991c0772df60f8bf1c27f
Michael Claesson <michael.claesson@consid.se>
2025-02-06 11:22:28 +0100

    Revert template change from auto formatting on save

--- source/php/Component/Notice/notice.blade.php
+++ source/php/Component/Notice/notice.blade.php
@@ -1,29 +1,31 @@
 <!-- notice.blade.php -->
 <div class="{{ $class }}" {!! $attribute !!} aria-labelledby="notice__text__{{ $id }}">
-
+    
     {{-- notice__ico --}}
-    @if ($icon)
-        <span class="{{ $baseClass }}__icon">
+    @if($icon)
+        <span class="{{$baseClass}}__icon">
             @icon(['icon' => $icon['name'], 'size' => 'md'])
             @endicon
         </span>
     @endif
-
+    
     {{-- notice__title --}}
-    @if ($message['title'])
+    @if($message['title'])
         @typography([
-            'variant' => 'h4',
-            'element' => 'h4',
-            'classList' => [$baseClass . '__title']
+            "variant" => "h4",
+            "element" => "h4",
+            'classList' => [
+                $baseClass . '__title'
+            ]
         ])
             {{ $message['title'] }}
         @endtypography
     @endif
 
     {{-- notice__text --}}
-    @if ($slotHasData || $message['text'])
-        <span id="notice__text__{{ $id }}" for="" class="{{ $baseClass }}__message">
-            @if (isset($message['text']))
+    @if($slotHasData || $message['text'])
+        <span id="notice__text__{{ $id }}" for="" class="{{$baseClass}}__message">
+            @if(isset($message['text']))
                 {!! $message['text'] !!}
             @endif
             {!! $slot !!}
@@ -31,35 +33,39 @@
     @endif
 
     {{-- notice__actions --}}
-    @if ($action || $dismissable)
-        <div class="{{ $baseClass }}__actions">
+    @if($action || $dismissable)
+        <div class="{{$baseClass}}__actions">
 
             {{-- notice__action --}}
-            @if ($action)
-                <div class="{{ $baseClass }}__action">
+            @if($action)
+                <div class="{{$baseClass}}__action">
                     @button([
                         'text' => $action['text'] ?? 'Undefined',
                         'href' => $action['url'] ?? '#',
                         'style' => 'basic',
                         'size' => 'sm',
-                        'classList' => [$baseClass . '__button'],
+                        'classList' => [
+                            $baseClass . '__button'
+                        ],
                     ])
                     @endbutton
                 </div>
             @endif
 
             {{-- notice__dismiss --}}
-            @if ($dismissable)
-                <div class="{{ $baseClass }}__dismiss">
+            @if($dismissable)
+                <div class="{{$baseClass}}__dismiss">
                     @button([
                         'icon' => 'close',
                         'style' => 'basic',
                         'size' => 'sm',
-                        'classList' => [$baseClass . '__dismiss'],
+                        'classList' => [
+                            $baseClass . '__dismiss'
+                        ],
                         'attributeList' => [
                             'data-dismissable-notice-trigger' => '1',
-                            'aria-label' => 'Close',
-                        ],
+                            'aria-label' => 'Close'
+                        ]
                     ])
                     @endbutton
                 </div>
