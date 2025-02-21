201d8227e3c85b9731d239234ab8d318cd45883c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-02-21 20:38:42 +0100

    fix: remove spaces in id of component.

--- source/php/Component/Nav/style/button.blade.php
+++ source/php/Component/Nav/style/button.blade.php
@@ -1,5 +1,5 @@
 @button([
-    'id' => $id . " - " . $item['id'] ."-" . $loop->index . "__label",
+    'id' => $id . "-" . $item['id'] ."-" . $loop->index . "__label",
     'icon' => isset($item['icon']['icon']) ? $item['icon']['icon'] : false,
     'reversePositions' => true,
     'text' => $item['label'],
