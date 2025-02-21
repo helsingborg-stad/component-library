83ff756f16a694b14a54c7d532c494797addfa61
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-02-21 21:17:54 +0100

    fix: correct xmlns

--- source/php/Component/Brand/brand.blade.php
+++ source/php/Component/Brand/brand.blade.php
@@ -3,7 +3,7 @@
 <div class="{{ $class }}" {!! $attribute !!}>
     <svg viewBox="0 0 500 100" preserveAspectRatio="xMinYMid meet" class="{{ $baseClass }}__viewbox">
         <foreignObject width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-            <div class="{{ $baseClass }}__container" xmlns="http://www.w3.org/2000/svg">
+            <div class="{{ $baseClass }}__container" xmlns="http://www.w3.org/1999/xhtml">
 
                 @if($logotype)
                     @logotype($logotype)
