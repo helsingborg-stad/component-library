a5a6832f43231b0347f166798eee81823da6c841
Michael Claesson <michael.claesson@consid.se>
2025-03-05 13:21:19 +0100

    Add images wrapper and move attributes to proper location

--- source/php/Component/Image/sub/mode/container.blade.php
+++ source/php/Component/Image/sub/mode/container.blade.php
@@ -7,18 +7,5 @@
     alt="{{$alt}}"
     style="{{$focus}}"
     {!! $imgAttributes !!}
-  >
+  />
 @endforeach
-
-<!-- Image styles -->
-<style>
-  @supports (container-type: inline-size) {
-    @foreach($containerQueryData as $item)
-      @foreach(['landscape', 'portrait'] as $dimension)
-        @container {{$item['media'][$dimension]}} and (orientation: {{$dimension}}) {
-            .{{$baseClass}}.{{$baseClass}}--container-query .{{$baseClass}}--{{$item['uuid']}} {display: block;}
-        }
-      @endforeach
-    @endforeach
-  }
-</style>
