421c105b5daa25d426fc05285cd11306f3075389
Michael Claesson <michael.claesson@consid.se>
2025-06-23 09:54:56 +0200

    Remove inline CSS

--- source/php/Component/Person/views/simple.blade.php
+++ source/php/Component/Person/views/simple.blade.php
@@ -3,27 +3,3 @@
 @include('Person.views.components.simple.details')
 
 @include('Person.views.components.simple.contact')
-
-<style>
-    .c-person--simple .c-person__contact .c-button {
-        min-width: 0;
-        height: auto;
-    }
-
-    .c-person--simple .c-person__image {
-        aspect-ratio: 1 / 1;
-    }
-
-    .c-person--simple .c-person__image img {
-        height: 100%;
-        object-fit: cover;
-        width: 100%;
-    }
-
-    .c-person--simple .c-avatar {
-        aspect-ratio: 1 / 1;
-        border-radius: 0;
-        height: 100%;
-        width: 100%;
-    }
-</style>
