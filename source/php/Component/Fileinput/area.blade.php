46da0fa213f623ee444c7ae0ef280951a54d2fd2
NiclasNorin <niclasn@outlook.com>
2025-07-31 15:39:02 +0200

    feat: fileinput preview

--- source/php/Component/Fileinput/area.blade.php
+++ source/php/Component/Fileinput/area.blade.php
@@ -42,47 +42,12 @@
   @endbutton
 
   <div class="{{$baseClass}}__file-list" data-js-file="list">
-    
-    <template data-js-file="listitem-template">
-      <div class="{{$baseClass}}__item" data-js-file="listitem" data-js-file-id="">
-        <div class="{{$baseClass}}__item-icon-wrapper">
-          @icon([
-            'icon' => 'attach_file',
-            'size' => 'sm',
-            'classList' => [
-              $baseClass . '__item-icon'
-            ],
-            'attributeList' => [
-              'aria-hidden' => 'true',
-              'data-js-file' => 'icon'
-            ]
-          ])
-          @endicon
-        </div>
-      
-        <div class="{{$baseClass}}__item-text">
-          <span class="{{$baseClass}}__item-name" data-js-file="filename"></span>
-          <span class="{{$baseClass}}__item-size" data-js-file="filesize"></span>
-        </div>
-
-        <div class="{{$baseClass}}__item-remove" data-tooltip="Remove file">
-          @button([
-            'size' => 'sm',
-            'style' => 'basic',
-            'icon' => 'delete',
-            'classList' => [
-              $baseClass . '__item-remove-button'
-            ],
-            'attributeList' => [
-              'aria-label' => $buttonRemoveLabel,
-              'data-js-file' => 'remove'
-            ]
-          ])
-          @endbutton
-        </div>
-      </div>
-    </template>
-</div>
+    @if($preview)
+        @include('Fileinput.partials.template.preview')
+    @else
+        @include('Fileinput.partials.template.list')
+    @endif
+  </div>
 
   <div
     class="{{$baseClass}}__filecounter"
