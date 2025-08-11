a4bfe44ae85e02caf3731d199c82fe7fb9142227
NiclasNorin <niclasn@outlook.com>
2025-08-11 11:23:10 +0200

    refactor: max file size for fileinput

--- source/php/Component/Fileinput/Fileinput.php
+++ source/php/Component/Fileinput/Fileinput.php
@@ -36,10 +36,18 @@ class Fileinput extends \ComponentLibrary\Component\BaseController
             $this->data['attributeList']['data-js-file-max'] = $this->filesMax;
         }
 
-        $this->data['acceptedFilesList'] = $this->createAcceptedFilesList($accept);
+        $acceptedTypesArray = is_array($accept) ? $accept : explode(',', $accept);
+        $this->data['acceptedFilesList'] = $this->createAcceptedFilesList($acceptedTypesArray);
+        
+        $maxFileSize = $this->determineMaxSize($maxSize, $acceptedTypesArray);
+
+        if ($maxFileSize) {
+            $this->data['attributeList']['data-js-file-max-size'] = $maxFileSize;
+        }
 
         // Indicate multiple or not
         $this->data['attributeList']['data-js-file-is-multi'] = $multiple;
+
 
         // Set class empty
         $this->data['classList'][] = "is-empty";
@@ -48,17 +56,68 @@ class Fileinput extends \ComponentLibrary\Component\BaseController
         $this->data['required'] = $required ?? false;
     }
 
-    private function createAcceptedFilesList($accept): string
+    private function determineMaxSize(int|string $maxSize, array $accept): ?string
     {
-        if (!is_string($accept)) {
-            return $accept;
+        // If numeric, return as is
+        if (is_numeric($maxSize)) {
+            return (string)$maxSize;
         }
 
+        // Define category size limits in MB
+        $sizePresets = [
+            'video' => [
+                'small' => '10',
+                'medium' => '30',
+                'large' => '50',
+            ],
+            'audio' => [
+                'small' => '5',
+                'medium' => '10',
+                'large' => '15',
+            ],
+            'image' => [
+                'small' => '2',
+                'medium' => '5',
+                'large' => '10',
+            ],
+            'document' => [
+                'small' => '1',
+                'medium' => '5',
+                'large' => '10',
+            ],
+        ];
+
+        $videoTypes = ['video/', 'mov', 'webm', 'mp4'];
+        $audioTypes = ['audio/', 'mp3', 'ogg', 'aac'];
+        $imageTypes = ['image/', 'jpg', 'jpeg', 'png', 'gif'];
+        $documentTypes = ['doc', 'docx', 'xls', 'xlsx', 'pdf'];
+
+        // Check if any of $accept items are in videoTypes, etc.
+        if (count(array_intersect($accept, $videoTypes)) > 0) {
+            return $sizePresets['video'][$maxSize] ?? $sizePresets['video']['large'];
+        } 
+        if (count(array_intersect($accept, $audioTypes)) > 0) {
+            return $sizePresets['audio'][$maxSize] ?? $sizePresets['audio']['large'];
+        }
+        if (count(array_intersect($accept, $imageTypes)) > 0) {
+            return $sizePresets['image'][$maxSize] ?? $sizePresets['image']['large'];
+        }
+        if (count(array_intersect($accept, $documentTypes)) > 0) {
+            return $sizePresets['document'][$maxSize] ?? $sizePresets['document']['large'];
+        }
+
+        return null;
+    }
+
+
+
+    private function createAcceptedFilesList(array $accept): string
+    {
         return( $this->data['lang']->allowedFiles ?? 'Allowed files') . ': ' . implode(' ', array_map(function($type) {
             if ($type === "video/*") return $this->data['lang']->videos ?? "Videos";
             if ($type === "image/*") return $this->data['lang']->images ?? "Images";
             if ($type === "audio/*") return $this->data['lang']->audios ?? "Audios";
             return $type;
-        }, explode(',', $accept)));
+        }, $accept));
     }
 }
