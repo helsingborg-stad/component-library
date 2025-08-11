c9bda48870e91a0336248a944474c96cefb92b61
Michael Claesson <michael.claesson@consid.se>
2025-08-11 19:53:34 +0200

    Setting for explicit link text and heading above image

---
+++ source/php/Component/Card/components/linkText.blade.php
@@ -0,0 +1,9 @@
+<p class="c-card__explicit-link u-display--flex u-flex--align-center u-gap-1">
+    {{ $linkText }}
+    @icon([
+        'icon' => 'arrow_right_alt',
+        'size' => 'md',
+        'color' => 'black'
+    ])
+    @endicon
+</p>
---
+++ source/php/Component/Card/partials/header.blade.php
@@ -0,0 +1,3 @@
+<div class="c-card__header">
+  @include('Card.components.heading')
+</div>
