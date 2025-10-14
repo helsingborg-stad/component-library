c0cd95144a063d347e1a812d85bd9af75a64e2ad
NiclasNorin <niclasn@outlook.com>
2025-10-14 11:30:24 +0200

    refactor: date-badge

--- source/php/Component/Datebadge/datebadge.blade.php
+++ source/php/Component/Datebadge/datebadge.blade.php
@@ -1,20 +1,9 @@
 <!-- datebadge.blade.php -->
 <div class="{{ $class }}" {!! $attribute !!}>
-    <div class="{{$baseClass . '__daymonth'}}">
-        @typography(['variant' => 'h1', 'element' => 'span', 'classList' => [$baseClass . '__date']])
-            {{ $day }}
-        @endtypography
-        @typography(['variant' => 'h4', 'element' => 'span', 'classList' => [$baseClass . '__month']])
-            {{ $month }}
-        @endtypography
-    </div>
-    @if($includeTime)
-        <div class="{{$baseClass . '__time'}}">
-            @icon(['icon' => 'access_time', 'size' => 'inherit'])
-            @endicon
-            @typography(['variant' => 'meta', 'element' => 'span'])
-                {{ $time }}
-            @endtypography
-        </div>
-    @endif
+    @typography(['element' => 'span', 'classList' => [$baseClass . '__month']])
+        {{ $month }}
+    @endtypography
+    @typography(['element' => 'span', 'classList' => [$baseClass . '__date']])
+        {{ $day }}
+    @endtypography
 </div>
