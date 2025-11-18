af181eef33769841ea9c7d56cddd6c36a6656260
NiclasNorin <niclasn@outlook.com>
2025-11-18 13:35:33 +0100

    fix: news item image always

--- source/php/Component/NewsItem/newsItem.blade.php
+++ source/php/Component/NewsItem/newsItem.blade.php
@@ -1,5 +1,5 @@
 <{{ $componentElement }} class="{{ $class }}" {!! $attribute !!}>
     @includeWhen($date || $subHeading || $headerLeftAreaSlotHasData || $headerRightAreaSlotHasData, 'NewsItem.components.header')
     @includeWhen($heading || $titleLeftAreaSlotHasData || $titleRightAreaSlotHasData, 'NewsItem.components.title')
-    @includeWhen($content || $contentLeftAreaSlotHasData || $contentRightAreaSlotHasData || $image || $hasPlaceholderImage, 'NewsItem.components.main')
+    @includeWhen($content || $contentLeftAreaSlotHasData || $contentRightAreaSlotHasData || $hasImage || $hasPlaceholderImage, 'NewsItem.components.main')
 </{{ $componentElement }}>
