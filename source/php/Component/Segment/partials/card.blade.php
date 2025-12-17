c4df07de2a897f4b6c596860707143c1bfee7964
NiclasNorin <niclasn@outlook.com>
2025-12-17 14:15:52 +0100

    test: linked segment card

--- source/php/Component/Segment/partials/card.blade.php
+++ source/php/Component/Segment/partials/card.blade.php
@@ -1,9 +1,9 @@
 <div class="{{$baseClass}}__content">
     @card([
-        'link' => $link ?? "",
+        'link' => empty($buttons) ? $link : false,
         'classList' => [$baseClass . '__content-card']
     ])
-    @include('Segment.components.content')
+    @include('Segment.components.content', ['link' => !empty($buttons) ? $link : false])
     @if ($isBlock)
         <div class="{{$baseClass}}__inner-blocks u-hide-empty">{!! '<InnerBlocks />' !!}</div>
     @endif
