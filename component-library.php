a025a0af9b43b73d8a9de35ed50e5490a9e94750
GitHub Action <>
2026-01-20 09:23:46 +0000

    build: update package file versions to 4.63.4

--- component-library.php
+++ component-library.php
@@ -4,7 +4,7 @@
  * Plugin Name:       component-library
  * Plugin URI:        (#plugin_url#)
  * Description:       A library of blade components
- * Version: 4.63.3
+ * Version: 4.63.4
  * Author:            Eric Rosenborg
  * Author URI:        (#plugin_author_url#)
  * License:           MIT
