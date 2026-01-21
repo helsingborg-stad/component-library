77d97862e5a11b389ea304d787edcd007b6dfdcc
Thor Brink <thor.brink@helsingborg.se>
2026-01-21 06:24:24 +0000

    fix(Select): make search placeholder configurable

--- source/php/Component/Select/partials/search.blade.php
+++ source/php/Component/Select/partials/search.blade.php
@@ -1,7 +1,7 @@
 @field([ 
     'type' => 'search',
     'icon' => ['icon' => 'search'],
-    'placeholder' => "Search",
+    'placeholder' => $searchPlaceholder,
     'classList' => [ $baseClass . '__search-field' ],
     'attributeList' => [ 'data-js-select-search-input' => true ],
     'size' => 'sm',
