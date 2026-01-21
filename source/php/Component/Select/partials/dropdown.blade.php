47d2d9edda91e99bb4bfbd90ceeeaf0318563981
Thor Brink <thor.brink@helsingborg.se>
2026-01-21 13:16:47 +0000

    fix(Select): move search field

--- source/php/Component/Select/partials/dropdown.blade.php
+++ source/php/Component/Select/partials/dropdown.blade.php
@@ -1,5 +1,6 @@
 <!-- Visual dropdown list -->
 <div class="{{$baseClass}}__dropdown" data-js-dropdown-element="true" aria-hidden="true">
+  @includeWhen($search, 'Select.partials.search')
   <ul class="{{$baseClass}}__options u-unlist u-padding--0 u-margin--0" role="listbox">
       @foreach ($options as $value => $name)
         @include('Select.partials.dropdown_item')
