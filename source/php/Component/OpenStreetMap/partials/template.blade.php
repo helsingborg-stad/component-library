5aa880cb7a8c4a1a3aed486da51559d26a572dd3
NiclasNorin <niclasn@outlook.com>
2026-03-02 16:58:46 +0100

    feat!: removing openstreetmap component

--- source/php/Component/OpenStreetMap/partials/template.blade.php
+++
@@ -1,6 +0,0 @@
-    <template class="{{$baseClass}}__pin-icon">
-        @include('OpenStreetMap.components.icon')
-    </template>
-    <template class="{{$baseClass}}__pin-tooltip">
-        @include('OpenStreetMap.components.tooltip')
-    </template>
