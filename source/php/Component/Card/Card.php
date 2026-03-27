e354ff48a863148b22df9d3ec78580713b4b36ad
NiclasNorin <niclasn@outlook.com>
2026-03-27 10:16:13 +0100

    feat: adding before content slot on card

--- source/php/Component/Card/Card.php
+++ source/php/Component/Card/Card.php
@@ -14,6 +14,7 @@ class Card extends \ComponentLibrary\Component\BaseController
 {
     private array $slotMapping = [
         'afterContent' => 'afterContentSlotHasData',
+        'beforeContent' => 'beforeContentSlotHasData',
         'floating'     => 'floatingSlotHasData',
         'aboveContent' => 'aboveContentSlotHasData',
         'belowContent' => 'belowContentSlotHasData',
--- source/php/Component/Card/card.blade.php
+++ source/php/Component/Card/card.blade.php
@@ -1,6 +1,7 @@
 <!-- card.blade.php -->
 <{{ $componentElement }} class="{{$class}}" {!! $attribute !!}>
     <div class="c-card__paint-container">
+        @includeWhen($beforeContentSlotHasData, 'Card.views.before-content')
         @includeWhen(!$slotHasData, 'Card.views.base')
         {!! $slot !!}
         @includeWhen($afterContentSlotHasData, 'Card.views.after-content')
