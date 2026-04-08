8edb1051cf7aef5b40fceea1096e8c014b8727fd
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2026-04-08 10:43:08 +0200

    fix: prefix each scope with s-

--- source/php/Component/Scope/Scope.php
+++ source/php/Component/Scope/Scope.php
@@ -8,7 +8,7 @@ class Scope extends \ComponentLibrary\Component\BaseController
         if(!empty($this->data['name'])) {
             if(is_array($this->data['name'])) {
                 $this->data['name'] = array_filter($this->data['name']); 
-                $this->data['name'] = implode(' ', $this->data['name']); 
+                $this->data['name'] = implode(' s-', $this->data['name']); 
             }
             
             $this->data['attributeList']['data-scope'] = 's-' . $this->data['name'] ;
