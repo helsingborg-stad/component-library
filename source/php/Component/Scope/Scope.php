804e04217da6d8972e986b17cb71905d4f71888e
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2026-04-13 14:13:44 +0200

    feat: delimited scope strings

--- source/php/Component/Scope/Scope.php
+++ source/php/Component/Scope/Scope.php
@@ -5,13 +5,19 @@ namespace ComponentLibrary\Component\Scope;
 class Scope extends \ComponentLibrary\Component\BaseController
 {
     public function init() {
-        if(!empty($this->data['name'])) {
-            if(is_array($this->data['name'])) {
-                $this->data['name'] = array_filter($this->data['name']); 
-                $this->data['name'] = implode(' s-', $this->data['name']); 
+        if (!empty($this->data['name'])) {
+            if (is_array($this->data['name'])) {
+                $scopes = array_filter($this->data['name']);
+    
+                $scopes = array_map(
+                    fn($scope) => 's-' . $scope . ';',
+                    $scopes
+                );
+    
+                $this->data['attributeList']['data-scope'] = implode(' ', $scopes);
+            } else {
+                $this->data['attributeList']['data-scope'] = 's-' . $this->data['name'] . ';';
             }
-            
-            $this->data['attributeList']['data-scope'] = 's-' . $this->data['name'] ;
         }
     }
 }
