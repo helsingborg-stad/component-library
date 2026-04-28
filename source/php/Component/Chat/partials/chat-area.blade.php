9aa587216820044973596a1b952bbb591678b115
NiclasNorin <niclasn@outlook.com>
2026-04-28 11:28:36 +0200

    chore: remove commented code

--- source/php/Component/Chat/partials/chat-area.blade.php
+++ source/php/Component/Chat/partials/chat-area.blade.php
@@ -14,34 +14,4 @@
     @chat__input([
     ])
     @endchat__input
-    {{-- @form([
-        'action' => '#',
-        'method' => 'POST',
-        'classList' => ['u-display--flex', 'u-flex-direction--column', 'u-gap-2'],
-        'attributeList' => ['data-js-chat-form' => true],
-        'validation' => false
-    ])
-        @field([
-            'type' => 'text',
-            'placeholder' => 'Type your message here...',
-            'multiline' => true,
-            'classList' => [
-                $baseClass . '__input'
-            ],
-            'attributeList' => [
-                'data-js-message-input' => true,
-            ],
-            'fieldAttributeList' => [
-                "style" => "resize: none;",
-            ]
-        ])
-        @endfield
-        @button([
-            'text' => 'send',
-            'color' => 'primary',
-            'style' => 'filled',
-            'type' => 'submit',
-        ])
-        @endbutton
-    @endform --}}
 @endelement
