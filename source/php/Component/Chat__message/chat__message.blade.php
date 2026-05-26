1ad1c722787e2a9edc1413dc44cd2cbe6ab5afcb
NiclasNorin <niclasn@outlook.com>
2026-05-26 16:35:05 +0200

    fix: chat message class

--- source/php/Component/Chat__message/chat__message.blade.php
+++ source/php/Component/Chat__message/chat__message.blade.php
@@ -2,11 +2,14 @@
     'classList' => $classList,
     'attributeList' => $attributeList
 ])
-<!-- This is a message component, it will be used as a template for both user messages and reply messages. -->
-   @element([
-    'attributeList' => [
-        'data-js-chat-message' => true
-    ]
+    <!-- This is a message component, it will be used as a template for both user messages and reply messages. -->
+    @element([
+        'attributeList' => [
+            'data-js-chat-message' => true
+        ],
+        'classList' => [
+            $baseClass . '__message',
+        ]
     ])
         <!-- Message content will be dynamically inserted here -->
         {!! $slot !!}
