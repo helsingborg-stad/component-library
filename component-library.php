8b5babf841d3d48cbc859784312ce71d237b815d
GitHub Action <>
2026-06-04 13:59:02 +0000

    build: update package file versions to 5.21.10

--- component-library.php
+++ component-library.php
@@ -4,7 +4,7 @@
  * Plugin Name:       component-library
  * Plugin URI:        (#plugin_url#)
  * Description:       A library of blade components
- * Version: 5.21.9
+ * Version: 5.21.10
  * Author:            Eric Rosenborg
  * Author URI:        (#plugin_author_url#)
  * License:           MIT
