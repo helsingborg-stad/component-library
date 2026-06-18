e526ddeed38790ef61bd47ca90bff84cb2558db1
Thor Brink <thor.brink@helsingborg.se>
2026-06-18 14:01:22 +0200

    fix: check card content for html to avoid empty p tags (#658)

--- source/php/Component/Card/components/content.blade.php
+++ source/php/Component/Card/components/content.blade.php
@@ -1,12 +1,16 @@
 @if($collapsible)
 <div class="{{$collapsible}}" data-js-toggle-class="{{$baseClass}}--collapse" data-js-toggle-item="{{$collpaseID}}">
 @endif
-    @typography([
-        'element' => $contentHtmlElement,
-        'classList' => [$baseClass . '__content'],
-    ])
+@if($contentHtmlElement === 'p')
+    @typography([ 'classList' => [$baseClass . '__content'], ])
         {!! $content !!}
     @endtypography
+@elseif($contentHtmlElement === 'div')
+    @element([ 'classList' => [$baseClass . '__content'], ])
+        {!! $content !!}
+    @endelement
+@endif
+
 @if($collapsible)
 </div>
 @endif
