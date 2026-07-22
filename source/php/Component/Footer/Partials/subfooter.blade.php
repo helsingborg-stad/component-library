ca6a93209093f7cea9ec673858462e0a362ee2da
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-07-22 12:07:14 +0200

    fix: remove logo scopes / add subfooter scope

--- source/php/Component/Footer/Partials/subfooter.blade.php
+++ source/php/Component/Footer/Partials/subfooter.blade.php
@@ -1,34 +1,36 @@
-<div class="{{$baseClass}}__subfooter {{$directionClass}} {{$alignmentClass}}">
-    <div class="o-container">
-        <div class="{{$baseClass}}__subfooter__wrapper">
-            @if($subfooterLogotype)
-                <div class="{{$baseClass}}__subfooter__logotype-wrapper">
-                    @logotype([
-                        'id' => 'footer-subfooter-logotype',
-                        'src'=> $subfooterLogotype,
-                        'alt' => $lang->goToHomepage ?? 'Go to homepage',
-                        'classList' => [$baseClass . '__subfooter__logotype'],
-                        'maskable' => true,
-                    ])
-                    @endlogotype
-                </div>
-            @endif
-            <ul class="{{$baseClass}}__subfooter__list">
-                @foreach($subfooter['content'] as $index => $item)
-                    <li>
-                        <strong>{{ $item['title'] }}</strong>
-                        @if($item['link'])
-                            @link([
-                                'href' => $item['link']
-                            ])
+@scope(['name' => ['footer']])
+    <div class="{{$baseClass}}__subfooter {{$directionClass}} {{$alignmentClass}}">
+        <div class="o-container">
+            <div class="{{$baseClass}}__subfooter__wrapper">
+                @if($subfooterLogotype)
+                    <div class="{{$baseClass}}__subfooter__logotype-wrapper">
+                        @logotype([
+                            'id' => 'footer-subfooter-logotype',
+                            'src'=> $subfooterLogotype,
+                            'alt' => $lang->goToHomepage ?? 'Go to homepage',
+                            'classList' => [$baseClass . '__subfooter__logotype'],
+                            'maskable' => true,
+                        ])
+                        @endlogotype
+                    </div>
+                @endif
+                <ul class="{{$baseClass}}__subfooter__list">
+                    @foreach($subfooter['content'] as $index => $item)
+                        <li>
+                            <strong>{{ $item['title'] }}</strong>
+                            @if($item['link'])
+                                @link([
+                                    'href' => $item['link']
+                                ])
+                                    {{ $item['content'] }}
+                                @endlink
+                            @else
                                 {{ $item['content'] }}
-                            @endlink
-                        @else
-                            {{ $item['content'] }}
-                        @endif
-                    </li>
-                @endforeach
-            </ul>
+                            @endif
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
-</div>
+@endscope
