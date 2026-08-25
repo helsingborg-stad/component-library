867c99796de0d306f7de84b7b93d0d94910d4e0d
NiclasNorin <niclasn@outlook.com>
2026-08-25 13:43:54 +0200

    feat: table heading

---
+++ source/php/Component/Table__header/Table__header.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ComponentLibrary\Component\Table__header;
+
+class Table__header extends \ComponentLibrary\Component\BaseController
+{
+    public function init()
+    {
+        extract($this->data);
+    }
+}
