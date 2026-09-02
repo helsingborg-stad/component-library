f32b61353d98c159ab3f0dd27f08f79d62d52741
NiclasNorin <niclasn@outlook.com>
2026-09-02 10:39:50 +0200

    fix: scroll container attribute

--- source/php/Component/Table/partials/footer.blade.php
+++ source/php/Component/Table/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="{{$baseClass}}__footer">
-    <div class="{{$baseClass}}__scroll-indicator-wrapper u-display--none">
+    <div class="{{$baseClass}}__scroll-indicator-wrapper u-display--none" data-js-table-scroll-indicator-container="1">
         <div class="{{$baseClass}}__scroll-indicator u-display--none" data-js-table-scroll-indicator="1">
         </div>
     </div>
