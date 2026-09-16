d117e0460994ad5b27ee58ff1eb4851db3905eaf
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2026-09-16 15:05:11 +0200

    refactor: add typed map configuration (#739)
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ source/php/Component/Map/MapData.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+namespace ComponentLibrary\Component\Map;
+final class MapData { public function __construct(public float|int $lat = 56.046467, public float|int $lng = 12.694512, public int $zoom = 11, public string $mapStyle = 'default', public string $height = '600px', public array $markers = [], public string $provider = 'openstreetmap') {} }
