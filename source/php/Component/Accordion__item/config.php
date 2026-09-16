d1167f4f272044dd7b978290bc1c4a12ca4bb814
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 10:45:19 +0200

    fix: retain accordion item icon dependency

--- source/php/Component/Accordion__item/config.php
+++ source/php/Component/Accordion__item/config.php
@@ -2,4 +2,4 @@
 declare(strict_types=1);
 use ComponentLibrary\Component\Accordion__item\AccordionItemData;
 use ComponentLibrary\ComponentConfiguration\ComponentConfig;
-return new ComponentConfig(slug: 'accordion__item', view: 'accordion__item.blade.php', data: AccordionItemData::class, dependencies: ['sass' => ['components' => ['accordion__item']]]);
+return new ComponentConfig(slug: 'accordion__item', view: 'accordion__item.blade.php', data: AccordionItemData::class, dependencies: ['sass' => ['components' => ['accordion__item', 'icon']]]);
