9446d0f7d0f018fcabf9ad7583f3eac1dfa2d2b9
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 13:21:51 +0200

    refactor: add typed slider configuration

---
+++ source/php/Component/Slider/SliderData.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+namespace ComponentLibrary\Component\Slider;
+final class SliderData { public function __construct(public bool|int $autoSlide = true, public bool $peekSlides = false, public bool $navigationHover = true, public string $ratio = '16:9', public bool $repeatSlide = true, public string $type = 'slide', public bool $heroStyle = false, public bool $shadow = true, public string|bool $customButtons = false, public array $arrowButtons = ['color'=>'primary','style'=>'filled'], public int $padding = 0, public int $gap = 2) {} }
