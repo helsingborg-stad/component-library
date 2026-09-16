10f32bba767ab85cd20a46fa8bbb3850c958591d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 11:02:09 +0200

    refactor: add typed option configuration

---
+++ source/php/Component/Option/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Option\OptionData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'option', view: 'option.blade.php', data: OptionData::class, dependencies: ['sass' => ['components' => ['option', 'icon']]]);
