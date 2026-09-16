d89a76be99d1179c90bb91c91ede40e9f21198ef
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 11:02:52 +0200

    refactor: add typed breadcrumb configuration

---
+++ source/php/Component/Breadcrumb/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Breadcrumb\BreadcrumbData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'breadcrumb', view: 'breadcrumb.blade.php', data: BreadcrumbData::class, dependencies: ['sass' => ['components' => ['breadcrumb', 'icon']]]);
