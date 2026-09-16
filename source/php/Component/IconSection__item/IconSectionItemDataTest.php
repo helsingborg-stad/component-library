9a7ecf1ebb2d2098dec6175f2bf08e607e1a14eb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 10:35:21 +0200

    refactor: add typed icon section item configuration

---
+++ source/php/Component/IconSection__item/IconSectionItemDataTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComponentLibrary\Component\IconSection__item;
+
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+use ComponentLibrary\ComponentConfiguration\ComponentDataReflector;
+use PHPUnit\Framework\TestCase;
+
+class IconSectionItemDataTest extends TestCase
+{
+    public function testTypedConfigDescribesTheIconSectionItemContract(): void
+    {
+        $config = require __DIR__ . '/config.php';
+
+        static::assertInstanceOf(ComponentConfig::class, $config);
+        static::assertSame('iconSection__item', $config->slug);
+        static::assertSame(IconSectionItemData::class, $config->data);
+        static::assertSame(['icon' => null], (new ComponentDataReflector())->getDefaultArguments(IconSectionItemData::class));
+    }
+}
