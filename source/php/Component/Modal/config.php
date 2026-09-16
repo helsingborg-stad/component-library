6ae77385607d4fb6580ca4a66d581785d241c56e
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2026-09-16 15:03:01 +0200

    refactor: add typed modal configuration (#752)
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

---
+++ source/php/Component/Modal/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Modal\ModalData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'modal', view: 'modal.blade.php', data: ModalData::class, dependencies: ['sass' => ['components' => ['modal','icon','typography','button']]]);
