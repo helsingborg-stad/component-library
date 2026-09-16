8396d6f8a74c4c7aaab202c09efb4e443b4a6fd2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 11:01:54 +0200

    refactor: add typed notification configuration

---
+++ source/php/Component/Notification/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Notification\NotificationData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'notification', view: 'notification.blade.php', data: NotificationData::class, dependencies: ['sass' => ['components' => ['notification', 'notice', 'button', 'icon']]]);
