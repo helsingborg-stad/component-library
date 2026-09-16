7c5c2b3f1ac6ae31ccdae6a818123285384d1397
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 12:27:13 +0200

    refactor: add typed footer configuration

---
+++ source/php/Component/Footer/FooterData.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+namespace ComponentLibrary\Component\Footer;
+final class FooterData { public function __construct(public string $componentElement = 'footer', public bool $slotOnly = false, public ?string $id = null, public string $logotype = '/', public string $logotypeHref = '', public array $links = [], public string $subfooterLogotype = '', public mixed $subfooter = null, public mixed $prefooter = null, public mixed $postfooter = null, public mixed $footerareas = null) {} }
