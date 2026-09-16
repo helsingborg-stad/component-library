88a68b6afa415efb71e8eb458cd628bb28c6bf7a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 10:18:35 +0200

    refactor: add typed card body configuration

---
+++ source/php/Component/Card__body/CardBodyData.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComponentLibrary\Component\Card__body;
+
+/**
+ * Typed input contract for the Card body area.
+ */
+final class CardBodyData
+{
+    public function __construct(
+        public string $slot = '',
+    ) {
+    }
+}
