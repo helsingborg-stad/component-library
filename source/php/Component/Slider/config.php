9fe3cee80bf96a4a53da17288280dac477e63c19
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 14:56:07 +0200

    fix: format.

--- source/php/Component/Slider/config.php
+++ source/php/Component/Slider/config.php
@@ -1,5 +1,17 @@
 <?php
+
 declare(strict_types=1);
+
 use ComponentLibrary\Component\Slider\SliderData;
 use ComponentLibrary\ComponentConfiguration\ComponentConfig;
-return new ComponentConfig(slug: 'slider', view: 'slider.blade.php', data: SliderData::class, dependencies: ['sass' => ['components' => ['slider','button','icon']]]);
+
+return new ComponentConfig(
+    slug: 'slider',
+    view: 'slider.blade.php',
+    data: SliderData::class,
+    dependencies: [
+        'sass' => [
+            'components' => ['slider', 'button', 'icon'],
+        ],
+    ],
+);
