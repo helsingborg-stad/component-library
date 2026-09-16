a91d22e8c4e05f7665aa2139b4f89a05e83b335b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 10:57:05 +0200

    refactor: add typed collection__item configuration

---
+++ source/php/Component/Collection__item/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Collection__item\CollectionItemData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'collection__item', view: 'collection__item.blade.php', data: CollectionItemData::class, dependencies: ['sass' => ['components' => ['collection', 'collection__item']]]);
