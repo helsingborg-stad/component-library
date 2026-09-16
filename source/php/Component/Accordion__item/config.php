b45babdbfcfde2f9b998b971dface1fe98b78572
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-09-16 10:44:53 +0200

    refactor: add typed accordion item configuration

---
+++ source/php/Component/Accordion__item/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+use ComponentLibrary\Component\Accordion__item\AccordionItemData;
+use ComponentLibrary\ComponentConfiguration\ComponentConfig;
+return new ComponentConfig(slug: 'accordion__item', view: 'accordion__item.blade.php', data: AccordionItemData::class, dependencies: ['sass' => ['components' => ['accordion__item']]]);
